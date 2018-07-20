6e7ea96304aef9e62e20c6f9b067165e19e629a2
Artem Oleksiuk <a.oleksuk@zoobio.de>
2018-07-20 17:24:21 +0300

    Fix error: Calling depends_on run is disabled for php71-mcrypt

--- Formula/php71-mcrypt.rb
+++ Formula/php71-mcrypt.rb
@@ -16,7 +16,7 @@ class Php71Mcrypt < AbstractPhp71Extension
   sha256 PHP_CHECKSUM[:sha256]
 
   depends_on "mcrypt"
-  depends_on "libtool" => :run
+  depends_on "libtool" => :build
 
   def install
     Dir.chdir "ext/mcrypt"
