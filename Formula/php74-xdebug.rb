67794b0951aaf77e11aff0442dffa89445d02cdd
scherepanov <sergey.cherepanov@ewave.com>
2019-07-18 02:23:28 +0400

    php74-xdebug

--- Formula/php74-xdebug.rb
+++ Formula/php74-xdebug.rb
@@ -4,8 +4,8 @@ class Php74Xdebug < AbstractPhp74Extension
   init
   desc "Provides debugging and profiling capabilities."
   homepage "https://xdebug.org"
-  url "https://xdebug.org/files/xdebug-2.6.0.tgz"
-  sha256 "b5264cc03bf68fcbb04b97229f96dca505d7b87ec2fb3bd4249896783d29cbdc"
+  url "https://xdebug.org/files/xdebug-2.8.0alpha1.tgz"
+  sha256 "28b773c233cb9af242bf8e518246bb171ed9cccfbef249a487c5c63d4c8a6165"
   head "https://github.com/xdebug/xdebug.git"
 
 
