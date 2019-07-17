7d6ccfff2f9c1080cfd6c75540034faa95f26b4e
scherepanov <sergey.cherepanov@ewave.com>
2019-07-17 15:40:11 +0400

    icu4c@64

--- Abstract/abstract-php.rb
+++ Abstract/abstract-php.rb
@@ -37,7 +37,7 @@ class AbstractPhp < Formula
     depends_on "freetype"
     depends_on "gettext"
     depends_on "gmp" => :optional
-    depends_on "icu4c"
+    depends_on "icu4c@64"
     depends_on "imap-uw" if build.include?("with-imap")
     depends_on "jpeg"
     depends_on "webp" => :optional if name.split("::")[2].downcase.start_with?("php7")
@@ -241,7 +241,7 @@ INFO
       "--with-gd",
       "--with-gettext=#{Formula["gettext"].opt_prefix}",
 #      ("--with-iconv-dir=/usr" if OS.mac?),
-      "--with-icu-dir=#{Formula["icu4c@63"].opt_prefix}",
+      "--with-icu-dir=#{Formula["icu4c@64"].opt_prefix}",
       "--with-jpeg-dir=#{Formula["jpeg"].opt_prefix}",
       ("--with-kerberos=/usr" if OS.mac?),
       "--with-mhash",
