cfd5f6b0f12000f3045422e5e9358db2a7c83405
scherepanov <sergey.cherepanov@ewave.com>
2019-07-18 02:27:56 +0400

    php74-yaml

--- Formula/php74-yaml.rb
+++ Formula/php74-yaml.rb
@@ -4,8 +4,8 @@ class Php74Yaml < AbstractPhp74Extension
   init
   desc "YAML-1.1 parser and emitter"
   homepage "https://pecl.php.net/package/yaml"
-  url "https://pecl.php.net/get/yaml-2.0.0.tgz"
-  sha256 "ef13ff56c184290c025a522bf9ae2e1b3ecc8543c3a5161dd02adec90897a221"
+  url "https://pecl.php.net/get/yaml-2.0.4.tgz"
+  sha256 "9786b0386e648f12cc18a038358bd57bee4906e350a2e9ab776d6a5f18fc6680"
   head "https://github.com/php/pecl-file_formats-yaml.git", :branch => "php7"
   revision 1
 
