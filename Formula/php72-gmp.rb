bc79ec0940babb74dcf8cff06d5dc222da1fb3ee
scherepanov <sergey.cherepanov@ewave.com>
2019-08-26 22:51:09 +0400

    gmp

---
+++ Formula/php72-gmp.rb
@@ -0,0 +1,27 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php72Gmp < AbstractPhp72Extension
+  init
+  desc "GMP core php extension"
+  homepage "https://php.net/manual/en/book.gmp.php"
+  revision 1
+
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+
+  depends_on "gmp"
+
+  def install
+    Dir.chdir "ext/gmp"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking",
+                           "--with-gmp=#{Formula["gmp"].opt_prefix}"
+    system "make"
+    prefix.install "modules/gmp.so"
+    write_config_file if build.with? "config-file"
+  end
+end
