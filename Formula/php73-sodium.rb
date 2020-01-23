d59f2cadd7937d97950d6f69a097d1e0236f727b
scherepanov <sergey@cherepanov.org.ua>
2020-01-23 14:00:15 +0400

    php73-sodium

---
+++ Formula/php73-sodium.rb
@@ -0,0 +1,24 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php73sodium < AbstractPhp73Extension
+  init
+  desc "GMP core php extension"
+  homepage "https://php.net/manual/en/book.sodium.php"
+  revision 1
+
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+
+  def install
+    Dir.chdir "ext/sodium"
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--disable-dependency-tracking"
+    system "make"
+    prefix.install "modules/sodium.so"
+    write_config_file if build.with? "config-file"
+  end
+end
