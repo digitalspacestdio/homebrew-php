fe7a344f204cae6eb8d1e4c980d293b6b653ef67
scherepanov <sergey@cherepanov.org.ua>
2020-01-23 14:03:12 +0400

    php72-sodium

--- Formula/php72-sodium.rb
+++ Formula/php72-sodium.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
-class Php73Sodium < AbstractPhp73Extension
+class Php72Sodium < AbstractPhp72Extension
   init
   desc "GMP core php extension"
   homepage "https://php.net/manual/en/book.sodium.php"
