ba6a722536f2d4d96acc8f58a6d24c85a1f8089a
scherepanov <sergey@cherepanov.org.ua>
2020-04-21 23:31:27 +0400

    update n98-magerun2

--- Formula/n98-magerun2.rb
+++ Formula/n98-magerun2.rb
@@ -4,8 +4,8 @@ class N98Magerun2 < AbstractPhpPhar
   init
   desc "Swiss army knife for Magento 2 developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun2-3.1.0.phar"
-  sha256 "00937d80a7ef87e56599a85430ee83214fc05143fbd07361c97c90c153d041f4"
+  url "https://files.magerun.net/n98-magerun2-4.1.0.phar"
+  sha256 "00d4b68e9e37fd3b452ed591c7891bdaff8c3881873c90883f9e49029c404165"
 
   bottle :unneeded
 
