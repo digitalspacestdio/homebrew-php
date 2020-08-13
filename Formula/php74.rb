aa7de5dddc2ff7db67093eeda721dc3da4b71e35
scherepanov <sergey.c@absoluteweb.com>
2020-08-13 13:56:40 +0400

    fix gd freetype

--- Formula/php74.rb
+++ Formula/php74.rb
@@ -5,7 +5,7 @@ class Php74 < AbstractPhp
   desc "PHP Version 7.4"
   include AbstractPhpVersion::Php74Defs
   version PHP_VERSION
-  revision 2
+  revision 3
 
   depends_on "pkg-config" => :build
   depends_on "krb5"
