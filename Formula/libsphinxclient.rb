50ac9648732cf5f4e3927de1ffe4d0610f0f4ab9
scherepanov <sergey.c@absoluteweb.com>
2020-09-18 16:46:38 +0400

    remove devel

--- Formula/libsphinxclient.rb
+++ Formula/libsphinxclient.rb
@@ -8,10 +8,10 @@ class Libsphinxclient < Formula
   head "https://github.com/sphinxsearch/sphinx.git"
 
 
-  devel do
-    url "http://sphinxsearch.com/files/sphinx-2.3.1-beta.tar.gz"
-    sha256 "0e5ebee66fe5b83dd8cbdebffd236dcd7cd33a7633c2e30b23330c65c61ee0e3"
-  end
+#   devel do
+#     url "http://sphinxsearch.com/files/sphinx-2.3.1-beta.tar.gz"
+#     sha256 "0e5ebee66fe5b83dd8cbdebffd236dcd7cd33a7633c2e30b23330c65c61ee0e3"
+#   end
 
   def install
     Dir.chdir "api/libsphinxclient"
--- Formula/php56-memcache.rb
+++ Formula/php56-memcache.rb
@@ -8,11 +8,11 @@ class Php56Memcache < AbstractPhp56Extension
   sha256 "73006c02194a5a7c196c6488d449e5f8c75573a73568fe1a94b15157c147305d"
   head "https://svn.php.net/repository/pecl/memcache/trunk/"
 
-
-  devel do
-    url "https://pecl.php.net/get/memcache-3.0.8.tgz"
-    sha256 "2cae5b423ffbfd33a259829849f6000d4db018debe3e29ecf3056f06642e8311"
-  end
+#
+#   devel do
+#     url "https://pecl.php.net/get/memcache-3.0.8.tgz"
+#     sha256 "2cae5b423ffbfd33a259829849f6000d4db018debe3e29ecf3056f06642e8311"
+#   end
 
   def install
     Dir.chdir "memcache-#{version}" unless build.head?
