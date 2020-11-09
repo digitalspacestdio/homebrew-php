1f88119b4ee592594abcf17418cb2349cf7c21c4
scherepanov <sergey@cherepanov.org.ua>
2020-11-09 17:47:00 +0400

    composer 1.10

--- Requirements/phar-requirement.rb
+++ Requirements/phar-requirement.rb
@@ -2,7 +2,8 @@ require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
 
 class PharRequirement < HomebrewPhpRequirement
   def satisfied?
-    `PATH="$HOMEBREW_PATH" /usr/bin/env php -m`.downcase.include? "phar"
+    @result = `PATH="$HOMEBREW_PATH" /usr/bin/env php -m`
+    @result.downcase.include? "phar"
   end
 
   def message
