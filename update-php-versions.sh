588de25a363e77425d5181e4e22902cc54c6be29
scherepanov <sergey@cherepanov.org.ua>
2021-07-04 19:24:31 +0400

    improve version updater

--- update-php-versions.sh
+++ update-php-versions.sh
@@ -22,6 +22,9 @@ export CONFIG='module Php'$(echo $PHP_VERSION | awk -F. '{ print $1 "" $2 }')'De
 perl -i -p0e 's/module Php'$(echo $PHP_VERSION | awk -F. '{ print $1 "" $2 }')'Defs.*?end/'\$ENV{"CONFIG"}'/s' ${DIR}/Abstract/abstract-php-version.rb
 done
 
-git add "Abstract/abstract-php-version.rb"
-git commit -m "php version update"
-git push
+if git status --porcelain 2>&1 | grep 'Abstract/abstract-php-version.rb'; then
+  git add "Abstract/abstract-php-version.rb"
+  git commit -m "php version update"
+  git push
+  bash Docker/build.sh
+fi
