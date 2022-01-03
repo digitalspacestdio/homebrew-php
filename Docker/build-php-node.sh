22756b62233f2a04d50ad644428603a4a07487af
scherepanov <sergey@cherepanov.org.ua>
2022-01-04 02:02:07 +0400

    php-node

---
+++ Docker/build-php-node.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+set -x
+cd $(dirname $0)
+repository=${1-digitalspacestudio}
+image=php-node
+formulas_php="digitalspacestdio/php/php80 digitalspacestdio/php/php74 digitalspacestdio/php/php73 digitalspacestdio/php/php72"
+formulas_node="node node@14 node@12"
+for php_formula in $formulas_php; do
+  for node_formula in $formulas_node; do
+    tag=$(echo $php_formula | md5 | cut -c-8)-$(echo $node_formula | md5 | cut -c-8)
+    docker build --rm --build-arg "BREW_FORMULA_PHP_PHP=$php_formula" --build-arg "BREW_FORMULA_NODE=$formulas_node" -t "$repository/$image:$tag" $image
+
+    php_version=$(docker run --rm "$repository/$image:$tag" php -v | egrep -o 'PHP [0-9][0-9]*\.[0-9][0-9]*\.[0-9][A-z0-9]*' | egrep -o '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][A-z0-9]*' )
+    php_major=$(echo $php_version | awk -F. '{print $1}')
+    php_minor=$(echo $php_version | awk -F. '{print $2}')
+    php_fix=$(echo $php_version | awk -F. '{print $3}')
+
+    node_version=$(docker run --rm "$repository/$image:$tag" node -v | egrep -o 'v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][A-z0-9]*' | egrep -o '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][A-z0-9]*' )
+    node_major=$(echo $node_version | awk -F. '{print $1}')
+    node_minor=$(echo $node_version | awk -F. '{print $2}')
+    node_fix=$(echo $node_version | awk -F. '{print $3}')
+
+    docker tag "$repository/$image:$tag" "$repository/$image:$php_major.$php_minor.$php_fix-$node_major.$node_minor.$node_fix"
+    docker tag "$repository/$image:$tag" "$repository/$image:$php_major.$php_minor.$php_fix-$node_major.$node_minor"
+    docker tag "$repository/$image:$tag" "$repository/$image:$php_major.$php_minor-$node_major.$node_minor"
+    docker tag "$repository/$image:$tag" "$repository/$image:$php_major.$php_minor-$node_major"
+
+    docker push "$repository/$image:$php_major.$php_minor.$php_fix-$node_major.$node_minor.$node_fix"
+    docker push "$repository/$image:$php_major.$php_minor.$php_fix-$node_major.$node_minor"
+    docker push "$repository/$image:$php_major.$php_minor-$node_major.$node_minor"
+    docker push "$repository/$image:$php_major.$php_minor-$node_major"
+  done
+done
