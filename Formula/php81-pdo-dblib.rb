87d8d10886a9beca4a2420e925519d15584b5c20
scherepanov <sergey@cherepanov.org.ua>
2022-01-04 18:22:14 +0400

    php 8.1

---
+++ Formula/php81-pdo-dblib.rb
@@ -0,0 +1,28 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php81PdoDblib < AbstractPhp81Extension
+  init
+  desc "Unified Sybase-DB style driver for PDO"
+  homepage "https://github.com/php/php-src/tree/master/ext/pdo_dblib"
+  revision 20
+
+
+  url PHP_SRC_TARBALL
+  sha256 PHP_CHECKSUM[:sha256]
+
+  depends_on "freetds"
+
+  def extension
+    "pdo_dblib"
+  end
+
+  def install
+    Dir.chdir "ext/pdo_dblib" unless build.head?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", "--with-pdo-dblib=#{Formula["freetds"].opt_prefix}", phpconfig
+    system "make"
+    prefix.install "modules/pdo_dblib.so"
+    write_config_file if build.with? "config-file"
+  end
+end
