781d71a92446983984b0de6ee94430753f2883d2
scherepanov <sergey@cherepanov.org.ua>
2022-01-10 08:35:13 +0400

    php-tidy-html5

--- Formula/php-tidy-html5.rb
+++ Formula/php-tidy-html5.rb
@@ -1,10 +1,10 @@
 class PhpTidyHtml5 < Formula
   desc "Granddaddy of HTML tools, with support for modern standards"
   homepage "https://www.html-tidy.org/"
-  url "https://github.com/htacg/php-tidy-html5/archive/5.8.0.tar.gz"
+  url "https://github.com/htacg/tidy-html5/archive/5.8.0.tar.gz"
   sha256 "59c86d5b2e452f63c5cdb29c866a12a4c55b1741d7025cf2f3ce0cde99b0660e"
   license "Zlib"
-  head "https://github.com/htacg/php-tidy-html5.git", branch: "next"
+  head "https://github.com/htacg/tidy-html5.git", branch: "next"
 
   livecheck do
     url :stable
