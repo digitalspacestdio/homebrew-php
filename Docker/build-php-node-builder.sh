0b0fe75ebb4837c449719025b7ac05d4b5b539ca
scherepanov <sergey@cherepanov.org.ua>
2022-01-11 04:11:45 +0400

    builder

---
+++ Docker/build-php-node-builder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+cd $(dirname $0)
+set -o allexport
+source .env
+set +o allexport
+repository=${1-digitalspacestudio}
+formulas_deps=$(docker run --rm $repository/linuxbrew sh -c "brew tap digitalspacestdio/common && brew tap digitalspacestdio/php && brew-list-build-deps $FORMULAS_PHP $FORMULAS_NODE $FORMULAS_EXTRA")
+
+# Assemble universal builder
+docker buildx build --push --platform linux/amd64,linux/arm64 $DOCKER_BUILD_BUILDER_ARGS \
+    --build-arg "BREW_FORMULA_DEPS=$formulas_deps" \
+    -t "$repository/php-node-builder" \
+    php-node-builder
+
+
--- Docker/build-php-node.sh
+++ Docker/build-php-node.sh
@@ -2,43 +2,51 @@
 set -e
 set -x
 cd $(dirname $0)
+set -o allexport
+source .env
+set +o allexport
 repository=${1-digitalspacestudio}
-formulas_php="digitalspacestdio/php/php80 digitalspacestdio/php/php74 digitalspacestdio/php/php73 digitalspacestdio/php/php72"
-formulas_node="node node@14 node@12"
-formulas_extra="mysql-client msmtp composer@2.2"
 versions_php=()
-formulas_deps=$(docker run --rm $repository/linuxbrew sh -c "brew tap digitalspacestdio/common && brew tap digitalspacestdio/php && brew-list-build-deps $formulas_php $formulas_node $formulas_extra")
-# Assemble universal builder
-docker build --rm \
-    --build-arg "BREW_FORMULA_DEPS=$formulas_deps" \
-    -t "$repository/php-node-builder" \
-    php-node-builder
 
-for formula_php in $formulas_php; do
+#if [[ -z $DOCKER_IMAGE_BUILDER ]]; then
+#  DOCKER_IMAGE_BUILDER="$repository/php-node-builder"
+#  if [[ -z $DOCKER_BUILD_SKIP_BUILDER ]]; then
+#    formulas_deps=$(docker run --rm $repository/linuxbrew sh -c "brew tap digitalspacestdio/common && brew tap digitalspacestdio/php && brew-list-build-deps $FORMULAS_PHP $FORMULAS_NODE $FORMULAS_EXTRA")
+#    # Assemble universal builder
+#    docker build $DOCKER_BUILD_BUILDER_ARGS --rm \
+#        --build-arg "BREW_FORMULA_DEPS=$formulas_deps" \
+#        -t "$repository/php-node-builder" \
+#        php-node-builder
+#  fi
+#fi
+
+for formula_php in $FORMULAS_PHP; do
     php_version=$(docker run --rm $repository/linuxbrew sh -c "brew tap digitalspacestdio/php && brew info ${formula_php} --json" | jq '.[].versions.stable' | egrep -o '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][A-z0-9]*')
     versions_php+=("$php_version")
     php_major=$(echo $php_version | awk -F. '{print $1}')
     php_minor=$(echo $php_version | awk -F. '{print $2}')
     php_fix=$(echo $php_version | awk -F. '{print $3}')
 
-    FROM_IMAGE_BUILDER="$repository/php-node-builder" \
-    FROM_IMAGE="$repository/linuxbrew" \
-    docker build --rm \
-    --build-arg "BREW_FORMULA_PHP='$formula_php'" \
-    -t "$repository/$image:$php_major.$php_minor.$php_fix" \
-    -t "$repository/$image:$php_major.$php_minor" \
+#    FROM_IMAGE_BUILDER=${DOCKER_IMAGE_BUILDER} \
+#    FROM_IMAGE="$repository/linuxbrew" \
+
+    docker build $DOCKER_BUILD_PHP_ARGS --rm \
+    --build-arg FROM_IMAGE_BUILDER="${DOCKER_IMAGE_BUILDER}" \
+    --build-arg FROM_IMAGE="$repository/linuxbrew" \
+    --build-arg BREW_FORMULA_PHP="$formula_php" \
+    -t "$repository/php:$php_major.$php_minor.$php_fix" \
+    -t "$repository/php:$php_major.$php_minor" \
     php
 done
 
-
 for php_version in $php_version; do
-  for formula_node in $formulas_node; do
+  for formula_node in $FORMULAS_NODE; do
     php_major=$(echo $php_version | awk -F. '{print $1}')
     php_minor=$(echo $php_version | awk -F. '{print $2}')
     php_fix=$(echo $php_version | awk -F. '{print $3}')
-    php_fix_major_minor_fix = "$repository/$image:$php_major.$php_minor.$php_fix";
-    php_fix_major_minor= "$repository/$image:$php_major.$php_minor";
-    php__major = "$repository/$image:$php_major";
+    php_fix_major_minor_fix = "$repository/php:$php_major.$php_minor.$php_fix";
+    php_fix_major_minor= "$repository/php:$php_major.$php_minor";
+    php__major = "$repository/php:$php_major";
 
     node_version=$(docker run --rm $repository/linuxbrew sh -c "brew info ${formula_node} --json" | jq '.[].versions.stable' | egrep -o '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][A-z0-9]*')
 
@@ -46,15 +54,18 @@ for php_version in $php_version; do
     node_minor=$(echo $node_version | awk -F. '{print $2}')
     node_fix=$(echo $node_version | awk -F. '{print $3}')
 
-    FROM_IMAGE_BUILDER="$repository/php-node-builder" \
-    FROM_IMAGE_PHP="$repository/$image:$php_version" \
-    FROM_IMAGE="$repository/linuxbrew" \
+#    FROM_IMAGE_BUILDER=${DOCKER_IMAGE_BUILDER} \
+#    FROM_IMAGE_PHP="$repository/php:$php_version" \
+#    FROM_IMAGE="$repository/linuxbrew" \
 
-    docker build --rm \
-    --build-arg "BREW_FORMULA_NODE=$formula_php" \
-    -t "$repository/$image:$php_major.$php_minor.$php_fix-$node_major.$node_minor.$node_fix" \
-    -t "$repository/$image:$php_major.$php_minor-$node_major.$node_minor" \
-    -t "$repository/$image:$php_major.$php_minor-$node_major" \
+    docker build $DOCKER_BUILD_NODE_ARGS --rm \
+    --build-arg FROM_IMAGE_BUILDER="${DOCKER_IMAGE_BUILDER}" \
+    --build-arg FROM_IMAGE_PHP="$repository/php:$php_version" \
+    --build-arg FROM_IMAGE="$repository/linuxbrew" \
+    --build-arg BREW_FORMULA_NODE="$formula_php" \
+    -t "$repository/php-node:$php_major.$php_minor.$php_fix-$node_major.$node_minor.$node_fix" \
+    -t "$repository/php-node:$php_major.$php_minor-$node_major.$node_minor" \
+    -t "$repository/php-node:$php_major.$php_minor-$node_major" \
     php-node
   done
 done
