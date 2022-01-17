69b89fc4bc20e085c78535a573de75686e93cf78
scherepanov <sergey@cherepanov.org.ua>
2022-01-17 16:41:54 +0400

    arm fix

--- Formula/phpmcrypt.rb
+++ Formula/phpmcrypt.rb
@@ -2,7 +2,7 @@ class Phpmcrypt < Formula
   desc "Replacement for the old crypt package and crypt(1) command"
   homepage "https://mcrypt.sourceforge.io"
   url "https://f001.backblazeb2.com/file/php-homebrew/mcrypt/mcrypt-2.6.8.tar.gz"
-  sha256 "fda960d3b8308096055dcbc7643e4fce792305e3f93aacb66b3c9ecbf4a4d63c"
+  sha256 "485e1861479272911f7832019d1b5d31873cde67b2e456bd984ce4bf5df532b1"
   license "GPL-3.0-or-later"
   revision 6
 
