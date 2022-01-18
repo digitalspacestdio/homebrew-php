fd636504c7c631f64866dbdfb9e9f3cb2cfb31ca
scherepanov <sergey@cherepanov.org.ua>
2022-01-18 05:46:04 +0400

    xhprof update

--- Formula/php74-xhprof.rb
+++ Formula/php74-xhprof.rb
@@ -4,10 +4,10 @@ class Php74Xhprof < AbstractPhp74Extension
   init
   desc "XHProf is a function-level hierarchical profiler for PHP and has a simple HTML based user interface."
   homepage "https://tideways.com"
-  url "https://codeload.github.com/tideways/php-xhprof-extension/tar.gz/afc61bd1e3336ef207247c5e92ae6b5dab0a6e1c"
-  sha256 "64f1c8e97e3480ccf329716bfc80ad4597d3f1a89f6f10c04c87c0a1443573a5"
+  url "https://codeload.github.com/tideways/php-xhprof-extension/tar.gz/ab391914cd59b95ea1a6904eacad6960a696a50b"
+  sha256 "1cfc8655b3561bd1b6ed778584f7e8ba36b7dac7cce1f3abb366093591dc526a"
   head "https://github.com/tideways/php-xhprof-extension.git", :branch => "master"
-  version "afc61bd"
+  version "5.0.4"
 
   def install
     safe_phpize
--- Formula/php80-xhprof.rb
+++ Formula/php80-xhprof.rb
@@ -4,10 +4,10 @@ class Php80Xhprof < AbstractPhp80Extension
   init
   desc "XHProf is a function-level hierarchical profiler for PHP and has a simple HTML based user interface."
   homepage "https://github.com/longxinH/xhprof"
-  url "https://codeload.github.com/longxinH/xhprof/tar.gz/3d256898fb4f8443fec43c7f4bd7ae439e8aeff2"
-  sha256 "c73b98a4f54dbee6570692a1ec6faef41653bb32bd02397a0229f458b99754c5"
+  url "https://codeload.github.com/longxinH/xhprof/tar.gz/ca4cde86ae6a7f40a1ddec044e4999d8a119e944"
+  sha256 "d9620cf3f1030b7da0390f4fd29161db18836541a866e6acde31c9e11a0ea04b"
   head "https://github.com/longxinH/xhprof.git", :branch => "master"
-  version "2.2.3"
+  version "2.3.5"
   revision 1
 
   def install
