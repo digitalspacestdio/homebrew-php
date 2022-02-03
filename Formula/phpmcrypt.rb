7c484c60a349fdeff217bce051e4a4467ea263a3
scherepanov <sergey@cherepanov.org.ua>
2022-02-04 01:44:50 +0400

    fix mcrypt under macOs

--- Formula/phpmcrypt.rb
+++ Formula/phpmcrypt.rb
@@ -4,21 +4,16 @@ class Phpmcrypt < Formula
   url "https://f001.backblazeb2.com/file/php-homebrew/mcrypt/mcrypt-2.6.8.tar.gz"
   sha256 "96dd3d5f42400a9918676ed1b7db87467589ba200410132f33766c52fbeefa35"
   license "GPL-3.0-or-later"
-  revision 8
+  revision 9
 
   keg_only "php only dependency"
 
   # Added automake as a build dependency to update config files in libmcrypt.
   # Please remove in future if there is a patch upstream which recognises aarch64 macos.
   depends_on "automake" => :build
-  depends_on "mhash"
+  depends_on "phplibmcrypt"
 
   uses_from_macos "zlib"
-
-  resource "libmcrypt" do
-    url "https://f001.backblazeb2.com/file/php-homebrew/mcrypt/libmcrypt-2.5.8.tar.gz"
-    sha256 "fda960d3b8308096055dcbc7643e4fce792305e3f93aacb66b3c9ecbf4a4d63c"
-  end
 
   # Patch to correct inclusion of malloc function on OSX.
   # Upstream: https://sourceforge.net/p/mcrypt/patches/14/
@@ -28,27 +23,13 @@ class Phpmcrypt < Formula
     # Work around configure issues with Xcode 12
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
 
-    resource("libmcrypt").stage do
-      # Workaround for ancient config files not recognising aarch64 macos.
-      %w[config.guess config.sub].each do |fn|
-       cp "#{Formula["automake"].opt_prefix}/share/automake-#{Formula["automake"].version.major_minor}/#{fn}", fn
-      end
-
-      args = []
-      args << "--prefix=#{prefix}"
-      args << "--mandir=#{man}"
-
-      system "./configure", *args
-      system "make", "install"
-    end
-
     # Workaround for ancient config files not recognising aarch64 macos.
     %w[config.guess config.sub].each do |fn|
       cp "#{Formula["automake"].opt_prefix}/share/automake-#{Formula["automake"].version.major_minor}/#{fn}", fn
     end
     args = []
     args << "--prefix=#{prefix}"
-    args << "--with-libmcrypt-prefix=#{prefix}"
+    args << "--with-libmcrypt-prefix=#{Formula["phplibmcrypt"].opt_prefix}"
     args << "-mandir=#{man}"
 
     system "./configure", *args
