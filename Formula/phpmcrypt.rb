9f0c2e8e07520b5faae61a84ee46967b34103b06
scherepanov <sergey@cherepanov.org.ua>
2022-02-04 02:09:09 +0400

    fix mcrypt under macOs

--- Formula/phpmcrypt.rb
+++ Formula/phpmcrypt.rb
@@ -1,19 +1,33 @@
 class Phpmcrypt < Formula
   desc "Replacement for the old crypt package and crypt(1) command"
   homepage "https://mcrypt.sourceforge.io"
-  url "https://f001.backblazeb2.com/file/php-homebrew/mcrypt/mcrypt-2.6.8.tar.gz"
-  sha256 "96dd3d5f42400a9918676ed1b7db87467589ba200410132f33766c52fbeefa35"
+  url "https://downloads.sourceforge.net/project/mcrypt/MCrypt/2.6.8/mcrypt-2.6.8.tar.gz"
+  sha256 "5145aa844e54cca89ddab6fb7dd9e5952811d8d787c4f4bf27eb261e6c182098"
   license "GPL-3.0-or-later"
-  revision 9
+  revision 10
+
+  bottle do
+      rebuild 4
+      sha256 cellar: :any, arm64_big_sur: "849ffa4e23dff9bff130c10d8ace02994034120b7d97ad36e9d7f6e8c048f97a"
+      sha256 cellar: :any, big_sur:       "e3182ac2f12baccfab81146bb4c6944b05154259a65165d694ca64e43d1f03f7"
+      sha256 cellar: :any, catalina:      "a52070083dfe080bbe0b8f71597a8a619c6b1421970c4670c6f40f5f2ba0fafe"
+      sha256 cellar: :any, mojave:        "6a23409a37396e2b2256485737a8195b06dcdea3607583e509f1d87d6a75faec"
+      sha256               x86_64_linux:  "33da6d2acf84ccd0db1ae1f9b82b0068a97bb6e8ba1de5dd310fc31c949ed432"
+  end
 
   keg_only "php only dependency"
 
   # Added automake as a build dependency to update config files in libmcrypt.
   # Please remove in future if there is a patch upstream which recognises aarch64 macos.
   depends_on "automake" => :build
-  depends_on "phplibmcrypt"
+  depends_on "mhash"
 
   uses_from_macos "zlib"
+
+  resource "libmcrypt" do
+    url "https://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz"
+    sha256 "e4eb6c074bbab168ac47b947c195ff8cef9d51a211cdd18ca9c9ef34d27a373e"
+  end
 
   # Patch to correct inclusion of malloc function on OSX.
   # Upstream: https://sourceforge.net/p/mcrypt/patches/14/
@@ -23,13 +37,27 @@ class Phpmcrypt < Formula
     # Work around configure issues with Xcode 12
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
 
+    resource("libmcrypt").stage do
+      # Workaround for ancient config files not recognising aarch64 macos.
+      %w[config.guess config.sub].each do |fn|
+       cp "#{Formula["automake"].opt_prefix}/share/automake-#{Formula["automake"].version.major_minor}/#{fn}", fn
+      end
+
+      args = []
+      args << "--prefix=#{prefix}"
+      args << "--mandir=#{man}"
+
+      system "./configure", *args
+      system "make", "install"
+    end
+
     # Workaround for ancient config files not recognising aarch64 macos.
     %w[config.guess config.sub].each do |fn|
       cp "#{Formula["automake"].opt_prefix}/share/automake-#{Formula["automake"].version.major_minor}/#{fn}", fn
     end
     args = []
     args << "--prefix=#{prefix}"
-    args << "--with-libmcrypt-prefix=#{Formula["phplibmcrypt"].opt_prefix}"
+    args << "--with-libmcrypt-prefix=#{prefix}"
     args << "-mandir=#{man}"
 
     system "./configure", *args
