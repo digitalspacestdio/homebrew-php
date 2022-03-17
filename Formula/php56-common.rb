d1755f36110384f7711ae7f1918a92ef21dd6aa2
scherepanov <sergey@cherepanov.org.ua>
2022-03-17 22:11:51 +0400

    supervisor

--- Formula/php56-common.rb
+++ Formula/php56-common.rb
@@ -5,7 +5,7 @@ class Php56Common < Formula
   desc "PHP Version 5.6 (Common Package)"
   include AbstractPhpVersion::Php56Defs
   version PHP_VERSION
-  revision 17
+  revision 18
 
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
@@ -109,7 +109,7 @@ class Php56Common < Formula
     if build.with? "supervisor"
       if config_file
         supervisor_config_dir.mkpath
-        supervisor_config_path.write(config_file)
+        supervisor_config_path.write(config_file) if !File.exist?(supervisor_config_path)
       end
     end
   end
