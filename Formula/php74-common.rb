9742a028e8e23006d47ff3e9f117fd2f18c5f94c
scherepanov <sergey@cherepanov.org.ua>
2022-03-19 22:20:41 +0400

    add pv

--- Formula/php74-common.rb
+++ Formula/php74-common.rb
@@ -5,7 +5,7 @@ class Php74Common < Formula
   desc "PHP Version 7.4 (Common Package)"
   include AbstractPhpVersion::Php74Defs
   version PHP_VERSION
-  revision 9
+  revision 10
 
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
