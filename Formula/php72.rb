73b551fb00b5b6a66a99c4f2acb78d6780fd81aa
scherepanov <sergey@cherepanov.org.ua>
2022-03-23 23:21:15 +0400

    without sqlite option

--- Formula/php72.rb
+++ Formula/php72.rb
@@ -5,7 +5,7 @@ class Php72 < AbstractPhp
   desc "PHP Version 7.2"
   include AbstractPhpVersion::Php72Defs
   version PHP_VERSION
-  revision 3
+  revision 4
   keg_only :versioned_formula
   depends_on "libjpeg"
 
