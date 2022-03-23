16203aaf9443233d3b1501d5030a831d6b1368cd
scherepanov <sergey@cherepanov.org.ua>
2022-03-23 13:33:52 +0400

    ignore inreplace error

--- Formula/php74-common.rb
+++ Formula/php74-common.rb
@@ -93,27 +93,39 @@ class Php74Common < Formula
   def install
     system "echo $(date) > installed.txt"
 
-    inreplace config_path_php do |s|
-        s.sub!(/^.*?short_open_tag\s*=.+$/, "short_open_tag = off")
-        s.sub!(/^.*?max_execution_time\s*=.+$/, "max_execution_time = 900")
-        s.sub!(/^.*?memory_limit\s*=.+$/, "memory_limit = 4096M")
-        s.sub!(/^.*?upload_max_filesize\s*=.+$/, "upload_max_filesize = 256M")
-        s.sub!(/^.*?post_max_size\s*=.+$/, "post_max_size = 256M")
-        s.sub!(/^.*?display_errors\s*=.+$/, "display_errors = on")
-        s.sub!(/^.*?error_reporting\s*=.+$/, "error_reporting = E_ALL ^ E_DEPRECATED")
-        s.sub!(/^.*?max_input_vars\s*=.+$/, "max_input_vars = 100000")
-        s.sub!(/^.*?display_startup_errors\s*=.+$/, "display_startup_errors = on")
-        s.sub!(/^.*?soap.wsdl_cache_ttl\s*=.+$/, "soap.wsdl_cache_ttl = 1")
+    begin
+        inreplace config_path_php do |s|
+            s.sub!(/^.*?short_open_tag\s*=.+$/, "short_open_tag = off")
+            s.sub!(/^.*?max_execution_time\s*=.+$/, "max_execution_time = 900")
+            s.sub!(/^.*?memory_limit\s*=.+$/, "memory_limit = 4096M")
+            s.sub!(/^.*?upload_max_filesize\s*=.+$/, "upload_max_filesize = 256M")
+            s.sub!(/^.*?post_max_size\s*=.+$/, "post_max_size = 256M")
+            s.sub!(/^.*?display_errors\s*=.+$/, "display_errors = on")
+            s.sub!(/^.*?error_reporting\s*=.+$/, "error_reporting = E_ALL ^ E_DEPRECATED")
+            s.sub!(/^.*?max_input_vars\s*=.+$/, "max_input_vars = 100000")
+            s.sub!(/^.*?display_startup_errors\s*=.+$/, "display_startup_errors = on")
+            s.sub!(/^.*?soap.wsdl_cache_ttl\s*=.+$/, "soap.wsdl_cache_ttl = 1")
+        end
+    rescue StandardError
+        nil
     end
 
-    inreplace config_path_php_fpm do |s|
-        s.sub!(/^.*?error_log\s*=.+$/, "error_log = /dev/stdout")
+    begin
+        inreplace config_path_php_fpm do |s|
+            s.sub!(/^.*?error_log\s*=.+$/, "error_log = /dev/stdout")
+        end
+    rescue StandardError
+        nil
     end
 
-    inreplace config_path_php_fpm_www do |s|
-        s.sub!(/^.*?user\s*=.+$/, "; user = #{user}")
-        s.sub!(/^.*?group\s*=.+$/, "; group = #{user_group}")
-        s.sub!(/^.*?listen\s*=.+$/, "listen = 127.0.0.1:9074")
+    begin
+        inreplace config_path_php_fpm_www do |s|
+            s.sub!(/^.*?user\s*=.+$/, "; user = #{user}")
+            s.sub!(/^.*?group\s*=.+$/, "; group = #{user_group}")
+            s.sub!(/^.*?listen\s*=.+$/, "listen = 127.0.0.1:9074")
+        end
+    rescue StandardError
+        nil
     end
 
     prefix.install "installed.txt"
