bee4ab2b6b9108ba4b1f0c039c8d0af01baabde1
scherepanov <sergey@cherepanov.org.ua>
2023-03-21 13:47:19 +0000

    composer 2.5

---
+++ Formula/composer@2.5.rb
@@ -0,0 +1,68 @@
+class ComposerAT24 < Formula
+  desc "Dependency Manager for PHP"
+  homepage "https://getcomposer.org/"
+  url "https://getcomposer.org/download/2.5.5/composer.phar"
+  sha256 "566a6d1cf4be1cc3ac882d2a2a13817ffae54e60f5aa7c9137434810a5809ffc"
+  license "MIT"
+  version "2.5.5"
+
+  depends_on "gpatch"
+  depends_on "git"
+  depends_on "curl"
+
+  livecheck do
+    url "https://getcomposer.org/download/"
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/composer\.phar}i)
+  end
+
+  def install
+    bin.install "composer.phar" => "composer"
+  end
+
+  test do
+    (testpath/"composer.json").write <<~EOS
+      {
+        "name": "homebrew/test",
+        "authors": [
+          {
+            "name": "Homebrew"
+          }
+        ],
+        "require": {
+          "php": ">=5.3.4"
+          },
+        "autoload": {
+          "psr-0": {
+            "HelloWorld": "src/"
+          }
+        }
+      }
+    EOS
+
+    (testpath/"src/HelloWorld/Greetings.php").write <<~EOS
+      <?php
+
+      namespace HelloWorld;
+
+      class Greetings {
+        public static function sayHelloWorld() {
+          return 'HelloHomebrew';
+        }
+      }
+    EOS
+
+    (testpath/"tests/test.php").write <<~EOS
+      <?php
+
+      // Autoload files using the Composer autoloader.
+      require_once __DIR__ . '/../vendor/autoload.php';
+
+      use HelloWorld\\Greetings;
+
+      echo Greetings::sayHelloWorld();
+    EOS
+
+    system "#{bin}/composer", "install"
+    assert_match(/^HelloHomebrew$/, shell_output("php tests/test.php"))
+  end
+end
