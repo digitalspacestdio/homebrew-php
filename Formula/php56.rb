5ae7f84941f40da5df3952f6d924af34e263a15e
scherepanov <sergey@cherepanov.org.ua>
2023-03-22 00:25:39 +0000

    libsodium fix

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -143,8 +143,215 @@ class Php56 < AbstractPhp
   end
 
   def install_args
-    super
+    # Prevent PHP from harcoding sed shim path
+    ENV["lt_cv_path_SED"] = "sed"
+
+    # Ensure system dylibs can be found by linker on Sierra
+    ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra
+
+#    libzip = Formula["libzip"]
+    #ENV["CFLAGS"] = "-Wno-error -I#{libzip.opt_include}"
+
+    args = [
+      "--prefix=#{prefix}",
+      "--localstatedir=#{var}",
+      "--sysconfdir=#{config_path}",
+      "--with-config-file-path=#{config_path}",
+      "--with-config-file-scan-dir=#{config_path}/conf.d",
+      "--mandir=#{man}",
+      "--enable-bcmath",
+      "--enable-calendar",
+      "--enable-opcache-file",
+      "--enable-exif",
+      "--enable-ftp",
+      "--enable-gd-native-ttf",
+      "--enable-mbregex",
+      "--enable-mbstring",
+      "--enable-shmop",
+      "--enable-soap",
+      "--enable-sockets",
+      "--enable-sysvmsg",
+      "--enable-sysvsem",
+      "--enable-sysvshm",
+      "--enable-wddx",
+#      "--enable-zip",
+      "--with-freetype-dir=#{Formula["freetype"].opt_prefix}",
+      "--with-gd",
+      "--with-gettext=#{Formula["gettext"].opt_prefix}",
+#      ("--with-iconv-dir=/usr" if OS.mac?),
+      ("--with-icu-dir=#{Formula["digitalspacestdio/common/icu4c@67.1"].opt_prefix}" if php_version.start_with?("7.0", "7.1", "7.2")),
+      ("--with-icu-dir=#{Formula["digitalspacestdio/common/icu4c@69.1"].opt_prefix}" if php_version.start_with?("5.6", "7.3")),
+      ("--with-icu-dir=#{Formula["digitalspacestdio/common/icu4c@72.1"].opt_prefix}" if php_version.start_with?("7.4", "8.0", "8.1", "8.2")),
+      ("--with-external-pcre" if !OS.mac? && !php_version.start_with?("7.4", "8.")),
+      ("--without-pcre-jit" if OS.mac?),
+      "--with-jpeg-dir=#{Formula["jpeg"].opt_prefix}",
+      ("--with-kerberos=/usr" if OS.mac?),
+      "--with-mhash",
+#      "--with-ndbm-dir=#{Formula["berkeley-db"].opt_prefix}",
+      "--with-png-dir=#{Formula["libpng"].opt_prefix}",
+      "--with-xmlrpc",
+      "--with-zlib=#{Formula["zlib"].opt_prefix}",
+#      "--with-libzip=#{Formula["libzip"].opt_prefix}",
+      "--with-readline=#{Formula["readline"].opt_prefix}",
+#      "--with-gdbm=#{Formula["gdbm"].opt_prefix}",
+      ("--with-iconv=#{Formula["libiconv"].opt_prefix}" if OS.mac?),
+      "--without-gmp",
+      "--without-snmp",
+    ]
+
+    if build.with?("homebrew-libxml2") || MacOS.version < :lion || MacOS.version >= :el_capitan
+      args << "--with-libxml-dir=#{Formula["libxml2"].opt_prefix}"
+    end
+
+    args << "--with-pdo-odbc=unixODBC,#{Formula["unixodbc"].opt_prefix}"
+    args << "--with-unixODBC=#{Formula["unixodbc"].opt_prefix}"
+
+    # Build with argon2 support (Password Hashing API)
+    if build.with?("argon2")
+      args << "--with-password-argon2=#{Formula["argon2"].opt_prefix}"
+    end
+
+    # Build Apache module by default
+    if build.with?("httpd")
+      args << "--with-apxs2=#{apache_apxs}"
+      args << "--libexecdir=#{libexec}"
+
+      unless build.with?("thread-safety")
+        inreplace "configure",
+          "APACHE_THREADED_MPM=`$APXS_HTTPD -V | grep 'threaded:.*yes'`",
+          "APACHE_THREADED_MPM="
+      end
+    end
+
+    #if build.with? "bz2"
+      args << "--with-bz2-dir=#{Formula["bzip2"].opt_prefix}" if OS.mac?
+    #end
+
+    if build.with? "debug"
+      args << "--enable-debug"
+    end
+
+    if build.with? "embed"
+      args << "--enable-embed=static"
+    end
+
+    if build.with? "enchant"
+      args << "--with-enchant=#{Formula["enchant"].opt_prefix}"
+    end
+
+    if build.with?("homebrew-libressl")
+      args << "--with-openssl=" + Formula["libressl"].opt_prefix.to_s
+    else
+      args << "--with-openssl=" + Formula[php_open_ssl_formula].opt_prefix.to_s
+    end
+
+    # Build PHP-FPM by default
+    if build_fpm?
+      args << "--enable-fpm"
+      args << "--with-fpm-user=_www"
+      args << "--with-fpm-group=_www"
+      (prefix+"var/log/php").mkpath
+      touch prefix+"var/log/php/php#{php_version}-fpm.log"
+#       plist_path.write plist
+#       plist_path.chmod 0644
+    elsif build.with? "cgi"
+      args << "--enable-cgi"
+    end
+
+    args << "--with-sqlite=#{Formula["sqlite"].opt_prefix}"
+    args << "--with-curl=#{Formula["digitalspacestdio/php/phpcurl"].opt_prefix}"
+    args << "--with-xsl=" + Formula["libxslt"].opt_prefix.to_s
+
+    if build.with? "imap"
+      args << "--with-imap=#{Formula["imap-uw"].opt_prefix}"
+      args << "--with-imap-ssl=" + Formula[php_open_ssl_formula].opt_prefix.to_s
+    end
+
+    unless build.without? "ldap"
+      args << "--with-ldap-dir=#{Formula["openldap"].opt_prefix}"
+      #args << "--with-ldap"
+      #args << "--with-ldap-sasl=/usr"
+    end
+
+    if build.with? "libmysql"
+      args << "--with-mysql-sock=/tmp/mysql.sock"
+      args << "--with-mysqli=#{HOMEBREW_PREFIX}/bin/mysql_config"
+      args << "--with-mysql=#{HOMEBREW_PREFIX}" unless (build.without? "legacy-mysql") || php_version.start_with?("7.")
+      args << "--with-pdo-mysql=#{HOMEBREW_PREFIX}"
+    elsif build.with? "mysql"
+      args << "--with-mysql-sock=/tmp/mysql.sock"
+      args << "--with-mysqli=mysqlnd"
+      args << "--with-mysql=mysqlnd" unless (build.without? "legacy-mysql") || php_version.start_with?("7.")
+      args << "--with-pdo-mysql=mysqlnd"
+    end
+
+    if build.with? "mssql"
+      args << "--with-mssql=#{Formula["freetds"].opt_prefix}"
+      args << "--with-pdo-dblib=#{Formula["freetds"].opt_prefix}"
+    end
+
+    # Do not build opcache by default; use a "phpxx-opcache" formula
+    # args << "--disable-opcache" if php_version.start_with?("5.5", "5.6", "7.")
+
+    if build.with? "pcntl"
+      args << "--enable-pcntl"
+    end
+
+    if build.with? "pdo-oci"
+      if ENV.key?("ORACLE_HOME")
+        args << "--with-pdo-oci=#{ENV["ORACLE_HOME"]}"
+      else
+        raise "Environmental variable ORACLE_HOME must be set to use --with-pdo-oci option."
+      end
+    end
+
+    if build.without? "pear"
+      args << "--without-pear"
+    end
+
+    if build.with? "postgresql"
+      if Formula["postgresql"].opt_prefix.directory?
+        args << "--with-pgsql=#{Formula["postgresql"].opt_prefix}"
+        args << "--with-pdo-pgsql=#{Formula["postgresql"].opt_prefix}"
+      else
+        args << "--with-pgsql=#{`pg_config --includedir`}"
+        args << "--with-pdo-pgsql=#{`which pg_config`}"
+      end
+    end
+
+    unless php_version.start_with?("5.3")
+      # dtrace is not compatible with phpdbg: https://github.com/krakjoe/phpdbg/issues/38
+      if build.without? "phpdbg"
+        args << "--enable-dtrace"
+        args << "--disable-phpdbg"
+      else
+        args << "--enable-phpdbg"
+
+        if build.with? "debug"
+          args << "--enable-phpdbg-debug"
+        end
+      end
+
+      args << "--enable-zend-signals"
+    end
+
+    if build.with? "webp"
+      args << "--with-webp-dir=#{Formula['webp'].opt_prefix}"
+    end
+
+    if build.with? "libvpx"
+      args << "--with-vpx-dir=#{Formula['libvpx'].opt_prefix}"
+    end
+
+    if build.with? "thread-safety"
+      args << "--enable-maintainer-zts"
+    end
+
+#     if build.with? "libsodium"
     args << "--with-sodium=#{Formula['libsodium'].opt_prefix}"
+#     end
+
+    args
   end
 
   def install
