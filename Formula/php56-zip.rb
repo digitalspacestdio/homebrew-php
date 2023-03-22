3c866f5f5cc0cd8b792faf4676bbe65f58b30db3
scherepanov <sergey@cherepanov.org.ua>
2023-03-22 02:13:13 +0000

    php56-zip

--- Formula/php56-zip.rb
+++ Formula/php56-zip.rb
@@ -9,6 +9,7 @@ class Php56Zip < AbstractPhp56Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  depends_on "zlib"
   depends_on "libzip"
   depends_on "pkg-config" => :build
 
@@ -26,7 +27,8 @@ class Php56Zip < AbstractPhp56Extension
                           phpconfig,
                           "--disable-dependency-tracking",
                           "--enable-zip",
-                          "--with-libzip=#{Formula["libzip"].opt_prefix}"
+                          "--with-libzip=#{Formula["libzip"].opt_prefix}",
+                          "--with-zlib-dir=#{Formula["zlib"].opt_prefix}"
     system "make"
     prefix.install "modules/zip.so"
     write_config_file if build.with? "config-file"
