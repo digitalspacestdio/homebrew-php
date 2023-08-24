f6f50f8bd51bd207dea27074d126613ef74e1e96
scherepanov <sergey@cherepanov.org.ua>
2023-08-24 02:07:49 +0000

    fix xhprof php 5.6

--- Formula/php56-xhprof.rb
+++ Formula/php56-xhprof.rb
@@ -3,27 +3,17 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php56Xhprof < AbstractPhp56Extension
   init
   desc "XHProf is a function-level hierarchical profiler for PHP and has a simple HTML based user interface."
-  homepage "https://github.com/facebook/xhprof"
-  url "https://github.com/facebook/xhprof/archive/254eb24dcfa763c76c57b472093ebc4b81af2b7d.tar.gz"
-  sha256 "c891ce1bf6730c67bd1dae6fb9b428e3adaf9c7b2008791199ba51a8556683ba"
-  head "https://github.com/facebook/xhprof.git"
-  version "254eb24"
-
-
+  head "https://github.com/tideways/php-xhprof-extension.git", branch: "4.x"
+  url "https://github.com/tideways/php-xhprof-extension/archive/refs/tags/v4.1.7.tar.gz"
+  sha256 "3e32ceacc9eec481e27b5df6d06de1e634294e2af9a64fe069bc686dba54fcb8"
+  version "4.1.7"
+  revision 1
   depends_on "pcre"
 
   def install
-    Dir.chdir "extension" do
-      # ENV.universal_binary if build.universal?
-
-      safe_phpize
-      system "./configure", "--prefix=#{prefix}",
-                            phpconfig
-      system "make"
-      prefix.install "modules/xhprof.so"
-    end
-
-    prefix.install %w[xhprof_html xhprof_lib]
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/tideways.so" => "tideways.so"
     write_config_file if build.with? "config-file"
   end
 end
