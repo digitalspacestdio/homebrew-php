d968d4e015e40a469114111f64a463e726532b51
scherepanov <sergey@cherepanov.org.ua>
2023-08-24 05:12:40 +0000

    modified:   Formula/php83-xhprof.rb

--- Formula/php83-xhprof.rb
+++ Formula/php83-xhprof.rb
@@ -4,11 +4,11 @@ class Php83Xhprof < AbstractPhp83Extension
   init
   desc "XHProf is a function-level hierarchical profiler for PHP and has a simple HTML based user interface."
   homepage "https://github.com/longxinH/xhprof"
-  url "https://codeload.github.com/longxinH/xhprof/tar.gz/ca4cde86ae6a7f40a1ddec044e4999d8a119e944"
-  sha256 "d9620cf3f1030b7da0390f4fd29161db18836541a866e6acde31c9e11a0ea04b"
   head "https://github.com/longxinH/xhprof.git", :branch => "master"
-  version "2.3.5"
-  revision 2
+  #url "https://codeload.github.com/longxinH/xhprof/tar.gz/ca4cde86ae6a7f40a1ddec044e4999d8a119e944"
+  #sha256 "d9620cf3f1030b7da0390f4fd29161db18836541a866e6acde31c9e11a0ea04b"
+  #version "2.3.5"
+  revision 1
 
   def install
     Dir.chdir "extension"
