aa19d0c3ac072068721e728f1fa50c19be66aa7f
scherepanov <sergey@cherepanov.org.ua>
2023-08-24 02:19:37 +0000

    fix xhprof php 5.6

--- Formula/php56-xhprof.rb
+++ Formula/php56-xhprof.rb
@@ -4,13 +4,14 @@ class Php56Xhprof < AbstractPhp56Extension
   init
   desc "XHProf is a function-level hierarchical profiler for PHP and has a simple HTML based user interface."
   head "https://github.com/tideways/php-xhprof-extension.git", branch: "4.x"
-  url "https://github.com/tideways/php-xhprof-extension/archive/refs/tags/v4.1.7.tar.gz"
-  sha256 "3e32ceacc9eec481e27b5df6d06de1e634294e2af9a64fe069bc686dba54fcb8"
+  url "https://github.com/tideways/php-xhprof-extension/archive/9b1c124a8cf75ed4ecf126c213b50c206df6a9db.tar.gz"
+  sha256 "9f84f5e8dca4b38921343a6e389da101e7d1e83ee0077220fd3f747cb67945a4"
   version "4.1.7"
   revision 1
   depends_on "pcre"
 
   def install
+    safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
     system "make"
     prefix.install "modules/tideways.so" => "tideways.so"
