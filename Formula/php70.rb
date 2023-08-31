40e619107505b95f7b17ca972c2d38b70d92d92b
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-01 03:45:12 +0400

    php70: add 7.0.33_23 bottle.

--- Formula/php70.rb
+++ Formula/php70.rb
@@ -6,6 +6,12 @@ class Php70 < AbstractPhp
   include AbstractPhpVersion::Php70Defs
   version PHP_VERSION
   revision 23
+
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
+    rebuild 1
+    sha256 arm64_ventura: "7baf0b182bbf3932cda7e1c19c7351a6394b2423c54f7620da9cb1ad9bf73e88"
+  end
   keg_only :versioned_formula
   include AbstractPhpVersion::Php70Defs
 
