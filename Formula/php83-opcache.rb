7e3074deecf09d641f4b5fe4c45b5658ea1b1ea9
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-01 04:46:33 +0400

    php83-opcache: add 8.3.0RC1_1 bottle.

--- Formula/php83-opcache.rb
+++ Formula/php83-opcache.rb
@@ -10,6 +10,12 @@ class Php83Opcache < AbstractPhp83Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c2176f1fafb2a3971664e241b78581e9ad4cef8fa1e39675f2a6953f55d519ea"
+  end
+
   depends_on "pcre"
 
   def extension_type
