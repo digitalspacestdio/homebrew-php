a725ebe63dfa6d64bef3ba84d82a395dce82fa31
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-01 04:56:06 +0400

    php82-opcache: add 8.2.10_1 bottle.

--- Formula/php82-opcache.rb
+++ Formula/php82-opcache.rb
@@ -10,6 +10,12 @@ class Php82Opcache < AbstractPhp82Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f29dfa425999c85fadf5ae6e1b83d3b4d2d8ec0d565e68ed2fe035d95bd30299"
+  end
+
   depends_on "pcre"
 
   def extension_type
