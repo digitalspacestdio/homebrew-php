23b6949ba5399c7990635d518c3ee9854318c1a7
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-01 04:46:41 +0400

    php83-zip: add 8.3.0RC1_1 bottle.

--- Formula/php83-zip.rb
+++ Formula/php83-zip.rb
@@ -10,6 +10,12 @@ class Php83Zip < AbstractPhp83Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
+    rebuild 1
+    sha256 cellar: :any, arm64_ventura: "6e310d1350ad98bca8f99439963b7f8ceb35c4cc55ddc4fdb0ed1ca955bd33d0"
+  end
+
   depends_on "libzip"
   depends_on "pkg-config" => :build
 
