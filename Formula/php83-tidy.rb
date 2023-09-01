b891c0d24237b65c76858f04227ca87d0aa8dbb7
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-01 04:46:40 +0400

    php83-tidy: add 8.3.0RC1_1 bottle.

--- Formula/php83-tidy.rb
+++ Formula/php83-tidy.rb
@@ -10,6 +10,12 @@ class Php83Tidy < AbstractPhp83Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
+    rebuild 1
+    sha256 cellar: :any, arm64_ventura: "9770d2cc187178bf10f55012ded17b6aa7cf8d4372a83d7f98ccd1c63e1fe24e"
+  end
+
   depends_on "digitalspacestdio/php/php-tidy-html5"
 
   def install
