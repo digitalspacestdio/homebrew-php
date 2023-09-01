748ba4ed2995299db36284f41ee55a012f0abd13
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-01 04:56:14 +0400

    php82-zip: add 8.2.10_1 bottle.

--- Formula/php82-zip.rb
+++ Formula/php82-zip.rb
@@ -10,6 +10,12 @@ class Php82Zip < AbstractPhp82Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
+    rebuild 1
+    sha256 cellar: :any, arm64_ventura: "4ee1c938c32aed88e8b1fbba164e1d9daafdff6f8c460260ae7f88da431fddba"
+  end
+
   depends_on "libzip"
   depends_on "pkg-config" => :build
 
