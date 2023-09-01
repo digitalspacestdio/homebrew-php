44583d7a146fefcd4015d16e193bb2b9359ebbb5
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-01 04:46:38 +0400

    php83-sodium: add 8.3.0RC1_1 bottle.

--- Formula/php83-sodium.rb
+++ Formula/php83-sodium.rb
@@ -9,6 +9,12 @@ class Php83Sodium < AbstractPhp83Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
+    rebuild 1
+    sha256 cellar: :any, arm64_ventura: "6dbdbe9a4dcef3ec9b6dfe35a1e98f8e8fe0bde5bc72f2850be826d808a36878"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libsodium"
 
