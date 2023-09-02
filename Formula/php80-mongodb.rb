6932735a91e793bc3644b037d050b34e0af930e2
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-02 14:37:04 +0400

    php80-mongodb: update 1.12.0_1 bottle.

--- Formula/php80-mongodb.rb
+++ Formula/php80-mongodb.rb
@@ -12,7 +12,8 @@ class Php80Mongodb < AbstractPhp80Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php80"
-    sha256 cellar: :any_skip_relocation, ventura: "c590bc93a7dbb259ec9bc8e481de97cb1ad0d6f8c6274b6072625a2b0e5ed311"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fc919eebebf37ca24a15b67b68922b5d65b75c45725392f11022d83a2631678d"
+    sha256 cellar: :any_skip_relocation, ventura:       "c590bc93a7dbb259ec9bc8e481de97cb1ad0d6f8c6274b6072625a2b0e5ed311"
   end
 
   depends_on "openssl"
