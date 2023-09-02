6168e2736095a0ffdf6581da842c3e779e545d72
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 04:11:27 +0000

    php81-sodium: add 8.1.23_1 bottle.

--- Formula/php81-sodium.rb
+++ Formula/php81-sodium.rb
@@ -9,6 +9,11 @@ class Php81Sodium < AbstractPhp81Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4151dbd8f91c9a07c6d9a68f20532e1508be8ec056da80db564bdd237f5b1871"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libsodium"
 
