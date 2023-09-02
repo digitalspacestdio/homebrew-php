bbba9e5068b4d88d6f272fecabdd0328e510ea7d
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 06:12:03 +0000

    php72-gmp: add 7.2.34_1 bottle.

--- Formula/php72-gmp.rb
+++ Formula/php72-gmp.rb
@@ -10,6 +10,12 @@ class Php72Gmp < AbstractPhp72Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "14504df79cbd6bdae0710100ae74b7744322c1fc723a2d629d4693a098edf7d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bdefbf20214b0ec820e3d4ae486460f5b2e133c03e3c7c5021866a0c51ba886f"
+  end
+
   depends_on "gmp"
 
   def install
