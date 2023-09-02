b3d80510d3c49657670058f0cd70d7bee4637f00
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:53:04 +0400

    php71-zip: add 7.1.33_1 bottle.

--- Formula/php71-zip.rb
+++ Formula/php71-zip.rb
@@ -8,6 +8,13 @@ class Php71Zip < AbstractPhp71Extension
 
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
+
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php71"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "65be5910a5063fb9e6a6607e3e6e93207344b50c41f28903efea0d8699681fa0"
+    sha256 cellar: :any_skip_relocation, ventura:       "7ae7982e69e86e25f9cabcac0e56455fadde467ade0b31f77dde995a914274cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "be286a0b4b9b28f62b3961819c4a1813d91966cbbca506c3729523a292afc18e"
+  end
   depends_on "libzip"
   depends_on "zlib"
   depends_on "pkg-config" => :build
