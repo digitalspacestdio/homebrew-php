978c5f8f29c5e4530cf927d7a9f60e90b2e5e335
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 06:12:51 +0000

    php72-pdo-pgsql: add 7.2.34_21 bottle.

--- Formula/php72-pdo-pgsql.rb
+++ Formula/php72-pdo-pgsql.rb
@@ -10,6 +10,12 @@ class Php72PdoPgsql < AbstractPhp72Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9a92c2b2beac9bd739e625a365d0a83c0c5cae1281b06aad8f515a6f75b3a3dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2bc2ed57f75639a69ecf8ffe8713bed70e972a371c4b35c4954417bdafd74dcd"
+  end
+
   depends_on "libpq"
 
   def extension
