f6ff5bd23338fa4f0ef659c6e8789cca66512461
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 03:50:21 +0000

    php82-ldap: update 8.2.10_1 bottle.

--- Formula/php82-ldap.rb
+++ Formula/php82-ldap.rb
@@ -13,7 +13,7 @@ class Php82Ldap < AbstractPhp82Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "333b4efc844c52cfe190438c4dbfa85e16dc0e9a48d1c969c059c8376ca7f21f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e53326f999faacdd8ffb4823020881a3dd0d403d48c8b0378ce248448c866e61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "271d784000543ba7556b6d46a7501c56130a2870d576c385dd00482cdf871584"
   end
 
   depends_on "openldap"
