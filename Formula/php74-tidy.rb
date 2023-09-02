554a809d497472e646d1cd6ce3507bdbf25e2cb2
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:45:08 +0400

    php74-tidy: update 7.4.33_19 bottle.

--- Formula/php74-tidy.rb
+++ Formula/php74-tidy.rb
@@ -13,6 +13,7 @@ class Php74Tidy < AbstractPhp74Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php74"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "9233991b768465bb12762e5e58aa03e7655c91086435659cb9d926bfe28657d2"
+    sha256 cellar: :any_skip_relocation, ventura:       "b843df1a83c6c7afd4bfa028af70a09f3e9ac851fdc45f892eab8a611b9521da"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "3bf61dfe254c1410056e35001b5fbba00fc92fe964aff03646982c35c1848dab"
   end
 
