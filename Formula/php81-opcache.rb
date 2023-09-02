e2476539bb826209d68f838ee06408c82fcc6eed
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:39:32 +0400

    php81-opcache: update 8.1.23_1 bottle.

--- Formula/php81-opcache.rb
+++ Formula/php81-opcache.rb
@@ -13,6 +13,7 @@ class Php81Opcache < AbstractPhp81Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php81"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "004b09aa0bc8a3a7d2ad9e1aab255dc381a5c83b43e1bda836b93430145ef949"
+    sha256 cellar: :any_skip_relocation, ventura:       "5bff939575df6723ef454b556b3eeaed3212717c7713c193c0a0463dbe31ef86"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "19548af3f7b86c28621135755ae0a2701a8741167a60dd0d2f741827c3cbea7c"
   end
 
