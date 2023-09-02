681427327bacc4bd4f6c917e2471dae7263c51f9
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:51:55 +0400

    php71-opcache: add 7.1.33_20 bottle.

--- Formula/php71-opcache.rb
+++ Formula/php71-opcache.rb
@@ -10,6 +10,13 @@ class Php71Opcache < AbstractPhp71Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php71"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "cb3c93932d3f531ad4540e0800dd892d668210a9a07ce567fa0bee3d0333e2b0"
+    sha256 cellar: :any_skip_relocation, ventura:       "d7f3551ed368bb48c751605abdc7dca155c7d0f654c06472d32422bf0684993c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f0cf95fbf5be16125fffb30afe401692e00428c69e02561b0a637031aef56da"
+  end
+
   depends_on "pcre"
 
   def extension_type
