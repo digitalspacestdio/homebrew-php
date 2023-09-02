4ee2e37205887b1ca5c261f91dbb6d45efc20b99
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:47:36 +0400

    php73-sodium: add 7.3.33_1 bottle.

--- Formula/php73-sodium.rb
+++ Formula/php73-sodium.rb
@@ -9,6 +9,13 @@ class Php73Sodium < AbstractPhp73Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php73"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1bc2f7443b3442cf4f4b027a22a9b71e46e5149c65e27ffd19a7a1ae818a2255"
+    sha256 cellar: :any_skip_relocation, ventura:       "1be1df60247c1a040ac8c79a396dc589b6bcdba5da6ae4a9bf8c59a17a879c29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e020f509e752920347cd0320c6d011df7cfcb039638f1a03c17651d6272dfb74"
+  end
+
   depends_on "libsodium"
 
   def install
