5e1f9ef908798cf9b0796f06993a09e9f6498b0f
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 06:13:16 +0000

    php72-zip: add 7.2.34_1 bottle.

--- Formula/php72-zip.rb
+++ Formula/php72-zip.rb
@@ -9,6 +9,12 @@ class Php72Zip < AbstractPhp72Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f86d6e4b07b528dc96cd1c233f1cfebcb0b4928f69650cc7d4e1ebd591cb789b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "094ffd9071a95419f009475b0ce9748007bfd7ec99096df9fdb11dbe461813bb"
+  end
+
   depends_on "libzip"
   depends_on "zlib"
   depends_on "pkg-config" => :build
