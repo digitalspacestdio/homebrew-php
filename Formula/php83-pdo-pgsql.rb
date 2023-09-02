6d9609f0c9a1656fb9746e9d52bc2ea79b6bdd1c
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 15:27:53 +0400

    php83-pdo-pgsql: update 8.3.0RC1_1 bottle.

--- Formula/php83-pdo-pgsql.rb
+++ Formula/php83-pdo-pgsql.rb
@@ -13,6 +13,7 @@ class Php83PdoPgsql < AbstractPhp83Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php83"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "ea612b8237868adbcd5fb7d22af2e438afa6be26de4f3e6c8719713d193d6d29"
+    sha256 cellar: :any_skip_relocation, ventura:       "0a5aa0f48b087a1baa3b0ab17e5d2cdc1be0a63328cd302abfb0a10a4a511e1f"
   end
 
   depends_on "libpq"
