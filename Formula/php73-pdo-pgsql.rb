60ae7f83b800e4ded47d93c919421d5945eeb511
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:47:10 +0400

    php73-pdo-pgsql: add 7.3.33_21 bottle.

--- Formula/php73-pdo-pgsql.rb
+++ Formula/php73-pdo-pgsql.rb
@@ -10,6 +10,13 @@ class Php73PdoPgsql < AbstractPhp73Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php73"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9d5453071b894bd95f78b209f8dd0d54188cb9fbdc47af69a3d69100c8d8c9ae"
+    sha256 cellar: :any_skip_relocation, ventura:       "59fe79462b6e0de0891d4e47a5c6f23bf9ae3ff6d550ce3e2a0b3f420b7bb350"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99ba3bdb5cdc9db96ce464a5eea0076078572893346f4296683bfaa63663db43"
+  end
+
   depends_on "libpq"
 
   def extension
