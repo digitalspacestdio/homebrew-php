816a246c3813083f786c9d4bfe0cb342cab8a3e4
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:35:00 +0400

    php82-opcache: update 8.2.10_1 bottle.

--- Formula/php82-opcache.rb
+++ Formula/php82-opcache.rb
@@ -13,6 +13,7 @@ class Php82Opcache < AbstractPhp82Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "eb3358fce939ada637aeaaec2b50b875ba7f1983168fe2247a730ab1e8ff5de5"
+    sha256 cellar: :any_skip_relocation, ventura:       "0f6965c233fa5042db884b872eb4290a95ddc6ad468c2d2a04d01cfe6b042f12"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "4bd1af160910089e7937cf36bfb944a7c2180eebb6fa29eca3b69a1b834dfe52"
   end
 
