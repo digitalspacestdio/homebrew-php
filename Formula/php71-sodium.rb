65956bbffd98a06fa107e6b20d63bda05b2a2af6
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:52:38 +0400

    php71-sodium: add 2.0.10 bottle.

--- Formula/php71-sodium.rb
+++ Formula/php71-sodium.rb
@@ -8,6 +8,13 @@ class Php71Sodium < AbstractPhp71Extension
   sha256 "2eebf3772d7441449b47abfe8f52043b9c6d6b5aff66aebd339c5d459d7fca28"
   head "https://github.com/jedisct1/libsodium-php.git"
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php71"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7d0f3acb3c59c9ef5d2737a33a4bf6801e85d958a5545edd5ef23e20a0625d4b"
+    sha256 cellar: :any_skip_relocation, ventura:       "20e7d7768354a08ee5b3aad490b3df7c1a24968efebc54a4c3ad1bb5df02c0ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a02d3d21aeceff1089f427362926930afdc010c2475ebed97f22dba1ec3a4475"
+  end
+
   depends_on "libsodium"
 
   def install
