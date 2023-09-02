e521b97a336f1fbcdcf7e2ca9f1c07a104e5d46a
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:42:18 +0400

    php80-pdo-pgsql: add 8.0.30_21 bottle.

--- Formula/php80-pdo-pgsql.rb
+++ Formula/php80-pdo-pgsql.rb
@@ -10,6 +10,11 @@ class Php80PdoPgsql < AbstractPhp80Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php80"
+    sha256 cellar: :any_skip_relocation, ventura: "93f6b00d84b6e2523762b774d1af673a6b27939a32ab17589cb4d39dd2fab2bf"
+  end
+
   depends_on "libpq"
 
   def extension
