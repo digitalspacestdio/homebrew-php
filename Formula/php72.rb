1ef6f4411bad90fe743463c735fed37bef04de23
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:48:19 +0400

    php72: update 7.2.34_5 bottle.

--- Formula/php72.rb
+++ Formula/php72.rb
@@ -10,6 +10,7 @@ class Php72 < AbstractPhp
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "3cd5e342922d3f919283869250b9841ea24f79de6e404d0ddb0852baeda3543f"
+    sha256 cellar: :any_skip_relocation, ventura:       "feccd1207478d7725a26bd6c928541e787502f3d81ead762a47b70b13ef3384b"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "fcd2387b571178151daff91685be03a563cb46eeabc2641bb717008adb042d46"
   end
   keg_only :versioned_formula
