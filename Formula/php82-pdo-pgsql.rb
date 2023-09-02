31db1788f109c211ce71639d4cf2ef5108531ca5
Unknown Developer <developer@magento3.mydevserver.local>
2023-09-02 01:36:23 +0000

    php82-pdo-pgsql: update 8.2.10_1 bottle.

--- Formula/php82-pdo-pgsql.rb
+++ Formula/php82-pdo-pgsql.rb
@@ -11,9 +11,8 @@ class Php82PdoPgsql < AbstractPhp82Extension
   sha256 PHP_CHECKSUM[:sha256]
 
   bottle do
-    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
-    rebuild 1
-    sha256 cellar: :any, arm64_ventura: "671908c180ef6207b5d92ce9ff9c8dcb1f8199cfb42916a7d32e2ca8695072d5"
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2a8bc8072bcfc119b1d07dba9af6ffddb9d2a9a06cad5232e16651f49cca8862"
   end
 
   depends_on "libpq"
