a0347dfeab5cd935c4e9b56af16804bf5d083bde
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:34:01 +0400

    php82-apcu: update 5.1.22_2 bottle.

--- Formula/php82-apcu.rb
+++ Formula/php82-apcu.rb
@@ -13,6 +13,7 @@ class Php82Apcu < AbstractPhp82Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "5ec6f9691d17b6729fc32f4d41d9b961e4501fdb46f9700a3fd408fd932c9910"
+    sha256 cellar: :any_skip_relocation, ventura:       "8ca1fc35350d59c970ef98929bbde607ef791d5119b1c227d3ae3f3eed25625c"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "4bb374b3db7eba42d04b26d7eb05d7bd7e16df241d74ac40807cf59bb3573e81"
   end
 
