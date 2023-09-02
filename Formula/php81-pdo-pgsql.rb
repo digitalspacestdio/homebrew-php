7fa04cf49c4a9c8f50a2e720ce43317fc7ea9df6
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-02 08:14:14 +0400

    php81-pdo-pgsql: update 8.1.23_1 bottle.

--- Formula/php81-pdo-pgsql.rb
+++ Formula/php81-pdo-pgsql.rb
@@ -12,7 +12,8 @@ class Php81PdoPgsql < AbstractPhp81Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php81"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ecf368d6721bf6cba7b74e2f09404aa5b17a2088d3a843ed5b0548188087f15c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "aab74e86f10e54d2228f586a4cc826314c58a748de3c22195f32c213d40ec78d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ecf368d6721bf6cba7b74e2f09404aa5b17a2088d3a843ed5b0548188087f15c"
   end
 
   depends_on "libpq"
