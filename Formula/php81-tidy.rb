7c7c0e15500402ecdb567413be4b1e5f5f809f50
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 04:11:35 +0000

    php81-tidy: add 8.1.23_1 bottle.

--- Formula/php81-tidy.rb
+++ Formula/php81-tidy.rb
@@ -10,6 +10,11 @@ class Php81Tidy < AbstractPhp81Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "005c092817a326911a1fbf3576c65190786ac08e0cb0996f0065f3db71ee19ce"
+  end
+
   depends_on "digitalspacestdio/php/php-tidy-html5"
 
   def install
