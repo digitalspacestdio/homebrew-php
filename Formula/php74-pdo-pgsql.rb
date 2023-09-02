43de48183533c0012b1a41a40439a23229dc23bc
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 05:26:39 +0000

    php74-pdo-pgsql: update 7.4.33_21 bottle.

--- Formula/php74-pdo-pgsql.rb
+++ Formula/php74-pdo-pgsql.rb
@@ -13,6 +13,7 @@ class Php74PdoPgsql < AbstractPhp74Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php74"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "cba44957fb5f47f7f5984127b593c02bb8b674ca89b47035830d5d681c727b5f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b1ac5d22987b6aca3c5cb107ab34916127ffdd028a0bf4930e62ac990dd1bbb7"
   end
 
   depends_on "libpq"
