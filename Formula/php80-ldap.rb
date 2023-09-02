32b11dd4e40afad671044ed33bdd7cf76d113379
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-02 14:36:55 +0400

    php80-ldap: update 8.0.30_26 bottle.

--- Formula/php80-ldap.rb
+++ Formula/php80-ldap.rb
@@ -12,7 +12,8 @@ class Php80Ldap < AbstractPhp80Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php80"
-    sha256 cellar: :any_skip_relocation, ventura: "43a0db7dffe9f8f75bc92c067c4dcc0571d2f31fa0466ee451025e93cbfeff32"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b2d09acc5d66f23030dc6b1e1a3401c94e14b79bf0f4c7791c0d0db1023f2a67"
+    sha256 cellar: :any_skip_relocation, ventura:       "43a0db7dffe9f8f75bc92c067c4dcc0571d2f31fa0466ee451025e93cbfeff32"
   end
 
   depends_on "openldap"
