a2be3253eeeb3f365e70fa18eb1e879ac70fdb93
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-02 14:37:50 +0400

    php80-tidy: update 8.0.30_19 bottle.

--- Formula/php80-tidy.rb
+++ Formula/php80-tidy.rb
@@ -12,7 +12,8 @@ class Php80Tidy < AbstractPhp80Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php80"
-    sha256 cellar: :any_skip_relocation, ventura: "4ececf0b0a95549e6f576491f97542ff8753d9ba4608832cb36eee0c6b579e20"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3f4414d191ecb75cefe7c938d1b4d7b6d0dd08953cfcf40b99669e808732efb5"
+    sha256 cellar: :any_skip_relocation, ventura:       "4ececf0b0a95549e6f576491f97542ff8753d9ba4608832cb36eee0c6b579e20"
   end
 
   depends_on "digitalspacestdio/php/php-tidy-html5"
