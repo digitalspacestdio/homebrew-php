0c0d5f7b4bc6b1bc8faba65d033466d44bc64347
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-02 08:12:49 +0400

    php81: update 8.1.23_1 bottle.

--- Formula/php81.rb
+++ Formula/php81.rb
@@ -9,7 +9,8 @@ class Php81 < AbstractPhp
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php81"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "854c795a04a774de86342a0ff900119ca1badc0fb1a2066597de24cf721fe86c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9545f7937f9135308035de6cc9d6f211df69d7702e221b370be2c79e1504d1d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "854c795a04a774de86342a0ff900119ca1badc0fb1a2066597de24cf721fe86c"
   end
   keg_only :versioned_formula
   depends_on "pkg-config" => :build
