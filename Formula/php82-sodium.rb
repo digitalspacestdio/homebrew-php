9ab74e9d82ab902fc1c030aefce21534e1f0ee52
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-02 07:08:56 +0400

    php82-sodium: update 8.2.10_1 bottle.

--- Formula/php82-sodium.rb
+++ Formula/php82-sodium.rb
@@ -11,8 +11,7 @@ class Php82Sodium < AbstractPhp82Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "53b779679cbce4059b7d500dcab0006f010816a5bc1ad7dea81a77246c6dad6c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae96c6edc0e193b0c8421a7e82ff186298401f4be04ba9d77b83bb40acd9ce6f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a9042f19de0e6a206e3547c944d7c1ea4078f1057c8f92d99fe2b21c6c0cf796"
   end
 
   depends_on "pkg-config" => :build
