e0c93c55442446b22cebe3cec4f4bb71be73eb18
Unknown Developer <developer@magento3.mydevserver.local>
2023-09-02 01:36:16 +0000

    php82-opcache: update 8.2.10_1 bottle.

--- Formula/php82-opcache.rb
+++ Formula/php82-opcache.rb
@@ -11,9 +11,8 @@ class Php82Opcache < AbstractPhp82Extension
   sha256 PHP_CHECKSUM[:sha256]
 
   bottle do
-    root_url "https://f003.backblazeb2.com/file/homebrew-bottles"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f29dfa425999c85fadf5ae6e1b83d3b4d2d8ec0d565e68ed2fe035d95bd30299"
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "26300adf94de6c39e9f8a7801a9835601ab05ff66a11f4125779dc68968082dd"
   end
 
   depends_on "pcre"
