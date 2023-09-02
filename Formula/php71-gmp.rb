23eb103ceac376122e78a482ba573af238dd7d1c
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 12:51:10 +0400

    php71-gmp: add 7.1.33_19 bottle.

--- Formula/php71-gmp.rb
+++ Formula/php71-gmp.rb
@@ -10,6 +10,13 @@ class Php71Gmp < AbstractPhp71Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php71"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4b0f94054110a35d03392689168fb38e9adf4dd9679690b1ee2b2ec3cc0dc2c4"
+    sha256 cellar: :any_skip_relocation, ventura:       "aac097cac48a812510067d3224479764f407b83ee757e0409e2403ca5abd3422"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68fd8e22aba28ebf03f866b094525269780a679fee5ad469335750b1b27a1a9e"
+  end
+
   depends_on "gmp"
 
   def install
