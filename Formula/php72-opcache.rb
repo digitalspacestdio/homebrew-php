e62317feab57b14b9278762a85a0c33f64b6bf7d
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 06:12:45 +0000

    php72-opcache: add 7.2.34_20 bottle.

--- Formula/php72-opcache.rb
+++ Formula/php72-opcache.rb
@@ -10,6 +10,12 @@ class Php72Opcache < AbstractPhp72Extension
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2b452c03088d29eb7982de11e243df39298679c049c3c6daecbe1aaf49a6091b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d97b6d3e64285aaeee85ef1e0d46a7874dadbbc508a6ea307074ae14ee1e54b"
+  end
+
   depends_on "pcre"
 
   def extension_type
