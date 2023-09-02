0e3459aaec393a92736a928d079b83491cbcb11c
scherepanov <sergey@cherepanov.org.ua>
2023-09-02 15:27:38 +0400

    php83-opcache: update 8.3.0RC1_1 bottle.

--- Formula/php83-opcache.rb
+++ Formula/php83-opcache.rb
@@ -13,6 +13,7 @@ class Php83Opcache < AbstractPhp83Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php83"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "fc843a96b5fb7c72e386094725b41ec8b40c542ba68201ccd7f835a090569ca8"
+    sha256 cellar: :any_skip_relocation, ventura:       "22ca40bf050f9feea9c64829cc5ca99c21976c82daf5bf43fb36ac5ef5efb98e"
   end
 
   depends_on "pcre"
