f2e141aecfafb70672458e571977c98834d456fc
scherepanov <scherepanov@digitalspace.studio>
2023-09-02 03:22:04 +0000

    php82-pdo-pgsql: update 8.2.10_1 bottle.

--- Formula/php82-pdo-pgsql.rb
+++ Formula/php82-pdo-pgsql.rb
@@ -13,6 +13,7 @@ class Php82PdoPgsql < AbstractPhp82Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "9aac477bfb5a9dc86abc4b92a483e21c65f799647298bc2160b9e483b3849416"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a8bc8072bcfc119b1d07dba9af6ffddb9d2a9a06cad5232e16651f49cca8862"
   end
 
   depends_on "libpq"
