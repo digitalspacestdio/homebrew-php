5262573493ecbcc1078c75747247dbe17d9bc5f0
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-21 20:24:30 +0400

    php56-gmp: update 5.6.40_6 bottle.

--- Formula/php56-gmp.rb
+++ Formula/php56-gmp.rb
@@ -10,7 +10,7 @@ class Php56Gmp < AbstractPhp56Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php56"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "186aa148428aa7b2254f874ee1f3c91453d070114ab610f1dc2db36da2b682e5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e17dd15ee9eba95d196a761b1fa640905729c1d0a80585de006d5b62e63e73c5"
     sha256 cellar: :any_skip_relocation, ventura:       "c10136f6c523875feef8e9a2fc788d2cdde113d32f9fc1c7809619f9d472a292"
   end
 
