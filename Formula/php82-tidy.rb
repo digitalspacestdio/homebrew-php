19fbd1a6246a37e7a7021de085612eb4e6df2e5f
Unknown Developer <developer@qwerty.devserver.local>
2023-12-21 12:22:23 +0000

    php82-tidy: update 8.2.13_1 bottle.

--- Formula/php82-tidy.rb
+++ Formula/php82-tidy.rb
@@ -12,9 +12,7 @@ class Php82Tidy < AbstractPhp82Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "621d0ee8b045d8e25e4926194ec3d982d02960d947b8f3c8bad979675fc28b58"
-    sha256 cellar: :any_skip_relocation, ventura:       "0593b37afcb5cb9faca614f56da1b41a51bb2a5707d9df3fb09ff8381d2efdc9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ef642b8af41e0e84f3847876e00dfddb5f49af7d289e60411027bc42afadc514"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "34bb66d5287b7fc73503635b72419af84dcd6af96b310ee92de5ba780ecf2792"
   end
 
   depends_on "digitalspacestdio/php/php-tidy-html5"
