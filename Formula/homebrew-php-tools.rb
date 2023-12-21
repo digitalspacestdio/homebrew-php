9ee11141a2d5303f8ae7f13d9edef8f5ac1aee04
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-21 19:59:34 +0400

    tools

--- Formula/homebrew-php-tools.rb
+++ Formula/homebrew-php-tools.rb
@@ -2,12 +2,14 @@ require 'formula'
 
 class HomebrewPhpTools < Formula
   url "https://github.com/digitalspacestdio/homebrew-php.git", :using => :git
+  version "0.1.0"
 
   depends_on "s3cmd"
 
   def install
-    bin.install "bin/_php-bottles-make-upload.sh" => "_php-bottles-make-upload"
-    bin.install "bin/_php-update-versions.sh" => "_php-update-versions"
+    libexec.install Dir["bin"]
+    bin.write_exec_script libexec/"_php-bottles-make-upload.sh"
+    bin.write_exec_script libexec/"_php-update-versions.sh"
   end
 
   def caveats
