0b26ba516617b9f94d525b631a0545e001860936
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-25 00:40:01 +0400

    php72-opcache: update 7.2.34_20 bottle.

--- Formula/php72-opcache.rb
+++ Formula/php72-opcache.rb
@@ -12,7 +12,7 @@ class Php72Opcache < AbstractPhp72Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b2da3732bc8088284519f3c69857fcf607c4ae102883bbfdc8813db861ac17c6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fffd2b3acf411d1337ced7221e52a10319c1c4dcc7329a5edb82f71d522e2e3c"
     sha256 cellar: :any_skip_relocation, ventura:       "a17cb25c2a8202d6f0788e42417310014540f2a87ec1832b441eb8c1d4def87d"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d97b6d3e64285aaeee85ef1e0d46a7874dadbbc508a6ea307074ae14ee1e54b"
   end
