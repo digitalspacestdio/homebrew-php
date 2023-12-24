f48c70701053a61fc23803b4341beeded8bab0a0
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-25 03:22:37 +0400

    php82-mongodb: update 1.15.1_2 bottle.

--- Formula/php82-mongodb.rb
+++ Formula/php82-mongodb.rb
@@ -12,7 +12,7 @@ class Php82Mongodb < AbstractPhp82Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5266c431a5725ede574e6c63451d49b15efdd88e61c7626ef3f6118907e1f21a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "dfd58adf6b44687f6fdbb20506ad760a5fbc5c98622f0d7d845364d879e6743d"
     sha256 cellar: :any_skip_relocation, ventura:       "afecea0ac70909a6c026b4a5ed6ba5a0160a39e382cadf6b9c75ab98745c6e63"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "886ff7f4af76cc52122672184d8da5c3543aaafdaf7bfd71a28739b0c1aeee2a"
   end
