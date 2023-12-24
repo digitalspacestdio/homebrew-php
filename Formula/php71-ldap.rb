69fd86a56bfd5e8c0dd064bda1033d65d9e01109
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 16:10:50 +0400

    php71-ldap: update 7.1.33_26 bottle.

--- Formula/php71-ldap.rb
+++ Formula/php71-ldap.rb
@@ -12,7 +12,7 @@ class Php71Ldap < AbstractPhp71Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php71"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "96b7fb7396da2a314393d9b8d0d20981b831125f022d8b8ba8e5194c97202f63"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4617a0b411297a4624a79c36da051e38f7f26f4b7d479ac9d6178756133e6a93"
     sha256 cellar: :any_skip_relocation, ventura:       "72bf9186ee41abc02a93ddf5860ec8a28bd8d048fa53ff6a035200c49c8d4ccb"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "61e1e3ca0d30036ce1ff180979f13cbf1e3176bca632b7e64d205d0784624e61"
   end
