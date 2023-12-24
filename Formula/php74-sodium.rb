433aaad56f7e9c8e4cf53c7314cd5a6de929fad9
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 21:34:25 +0400

    php74-sodium: update 7.4.33_1 bottle.

--- Formula/php74-sodium.rb
+++ Formula/php74-sodium.rb
@@ -11,7 +11,7 @@ class Php74Sodium < AbstractPhp74Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php74"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "dc825ebf7b0218b798e630d685401ddd2b7032e84e7ad0b44a976a8dab40e811"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "96baf59219656f7a3717bc532b58ed11bddcacae444c7089aa8435383c7e9984"
     sha256 cellar: :any_skip_relocation, ventura:       "dacc1966d6227c1e41e2c146eae68ad3bc9a6db1ab7d92b4a725f6fedca990f6"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d65b41de1c952d9dfc118be99986c09970217b8f2fb5058704f25175eabe4b65"
   end
