370547804447f6b46e6d0d1f13eb6f0713f04ffd
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-25 03:24:10 +0400

    php83-apcu: update 5.1.22_2 bottle.

--- Formula/php83-apcu.rb
+++ Formula/php83-apcu.rb
@@ -12,7 +12,7 @@ class Php83Apcu < AbstractPhp83Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php83"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b679a2a9edb63b4cb0e26f12ac9621bbb6d73cc43343ea151de55e5cb15dcf7f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4c4462701aae9010825f989eae86ac8b4367b66bcedb5fde8c101ae70a339ddd"
     sha256 cellar: :any_skip_relocation, ventura:       "dd7c3d3072d30942eb4ccc1931965bd8cd98dce65df8540fdfba7df5dac0f3e6"
   end
 
