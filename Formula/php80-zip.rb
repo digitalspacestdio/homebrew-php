4c3bbd804a03fcac592c7ee880d84929493a576a
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 21:21:35 +0400

    php80-zip: update 8.0.30_1 bottle.

--- Formula/php80-zip.rb
+++ Formula/php80-zip.rb
@@ -12,7 +12,7 @@ class Php80Zip < AbstractPhp80Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php80"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "aacdfd5106fc01dc6007422b52548f15a50d9185829d8d57966d14f86660507f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3290c957fa6dc838184b44606a52b60e4eb0b7bd4bb165686d6469509218bfac"
     sha256 cellar: :any_skip_relocation, ventura:       "94f16ba6261e559fb5e16805c9bb40e36a920395edc8f5776b3050bb57907788"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "24ca32fd1533d256c5efbead7d8655b99939ccd2b92c22a9f66a4f7dbe859316"
   end
