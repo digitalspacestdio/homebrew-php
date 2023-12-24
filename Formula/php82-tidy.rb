fef0706bd1c66b1d05dce23760e44d4f2c331319
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 17:23:50 +0400

    php82-tidy: update 8.2.13_1 bottle.

--- Formula/php82-tidy.rb
+++ Formula/php82-tidy.rb
@@ -12,7 +12,7 @@ class Php82Tidy < AbstractPhp82Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "044d6fb57eedef6997227af624103ec0b9db8f690bc13bd35a63bbff0621572a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a0a3ea295f16d2271a604180945e844d0931d62832679d9469bd9746aff943b8"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "34bb66d5287b7fc73503635b72419af84dcd6af96b310ee92de5ba780ecf2792"
   end
 
