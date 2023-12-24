dad360eb2d302df49e40ee39a545f19148b5ae03
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 23:07:47 +0400

    modified:   Formula/bottle-php-tools.rb

--- Formula/bottle-php-tools.rb
+++ Formula/bottle-php-tools.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class BottlePhpTools < Formula
   url "https://github.com/digitalspacestdio/homebrew-php.git", :using => :git
-  version "0.1.4"
+  version "0.1.5"
 
   depends_on "s3cmd"
   depends_on "jq"
