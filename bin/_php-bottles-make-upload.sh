83e2fe3e5419d6ec2234700b483e0ab9d24181b6
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 15:42:38 +0400

    bottles

--- bin/_php-bottles-make-upload.sh
+++ bin/_php-bottles-make-upload.sh
@@ -5,22 +5,11 @@ pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
 cd "${DIR}"
 
 FORMULAS=${@:-$(brew search digitalspacestdio/php | grep 'php[7-9]\{1\}[0-9]\{1\}$' | awk -F'/' '{ print $3 }' | sort)}
-echo $FORMULAS
 
-for formula in $(brew search digitalspacestdio/php | grep 'php[7-9]\{1\}[0-9]\{1\}$' | awk -F'/' '{ print $3"" }' | sort); do
+for formula in "$FORMULAS"; do
     echo "---> Starting $formula"
     ./_php-bottles-make.sh $formula && {
         ./_php-bottles-upload.sh $formula || echo "Failed to upload bottles for $formula"
     } || echo "Failed to build bottles for $formula"
     echo "---> Finished $formula"
 done
-
-# if [[ -z $1 ]]; then
-#     for line in $(brew search digitalspacestdio/php | grep 'php[7-9]\{1\}[0-9]\{1\}$' | awk -F'/' '{ print $3"" }' | sort); do
-#         ./_php-bottles-make.sh $line && {
-#             ./_php-bottles-upload.sh $line || echo "Failed to upload bottles for $line"
-#         } || echo "Failed to build bottles for $line"
-#     done
-# else
-#     ./_php-bottles-make.sh $1 && ./_php-bottles-upload.sh $1 || echo "Failed to create bottles for $1"
-# fi
