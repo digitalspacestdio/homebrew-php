35a995624d9ad5a3f8b9fac1742660a74633035f
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-25 03:22:00 +0400

    php82-gmp: update 8.2.14_1 bottle.

--- Formula/php82-gmp.rb
+++ Formula/php82-gmp.rb
@@ -12,8 +12,7 @@ class Php82Gmp < AbstractPhp82Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0eea752ec116974630beffeec3ee42d38f90e88b5e0d32d03e1c519224febc9a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bea92cceb86cc9998096b530f17cd3e2cbfbdfc74edd8733da9595ad0b95600a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "150c672d365afc3ff7da7935f7905680fb5b36093437bf541f31e239cb000c85"
   end
 
   depends_on "gmp"
