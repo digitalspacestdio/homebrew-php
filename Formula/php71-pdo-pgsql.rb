c5fc9ccda09cac2d2a58a7abe4a3fe1866d8fbe5
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-25 00:37:55 +0400

    php71-pdo-pgsql: update 7.1.33_21 bottle.

--- Formula/php71-pdo-pgsql.rb
+++ Formula/php71-pdo-pgsql.rb
@@ -12,7 +12,7 @@ class Php71PdoPgsql < AbstractPhp71Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php71"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "2283342fc43ba2b0b003fe41489541972a9be786ec2e1d8c5aef2da2c6dcc56f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "709bbef68dc4d2f35669cf26b510cca9954e222435afd99d193e652ab1652a3f"
     sha256 cellar: :any_skip_relocation, ventura:       "fda12de570ed71443a5d993578c62e0fd3dee305ac176002ba79af4a0fcd5c2f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b0b5e420c93196a48355d1d664afabcc6d789b84f1cb8897ba7f3e298bddfd7"
   end
