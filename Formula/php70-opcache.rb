829321b32721075679db452d0103c39b01d9e76a
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 14:41:51 +0400

    php70-opcache: update 7.0.33_20 bottle.

--- Formula/php70-opcache.rb
+++ Formula/php70-opcache.rb
@@ -11,7 +11,7 @@ class Php70Opcache < AbstractPhp70Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php70"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6a7b57a897471c874a7da6c62979cd457ef2fcc427260e94a660076c42734e45"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2c329454d2219ce0e03d80c7cbe0ed93fd3e47b1329d5237531e63affb6899b5"
     sha256 cellar: :any_skip_relocation, ventura:       "377bac8b204d9e0b45302e379070c4bdee3d0474f68df0b5b4bdeb0596a6caca"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a132840b69ad5c9511e0c95a0bbdc78fb9f303aa44511c39f90b7473f1541b97"
   end
