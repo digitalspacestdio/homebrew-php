e3f9cc0f49e9544e201b02d96b6af003edcef84b
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 14:27:46 +0400

    exclude php56

--- bin/_php-bottles-make-upload.sh
+++ bin/_php-bottles-make-upload.sh
@@ -6,7 +6,7 @@ cd "${DIR}"
 if [[ -z $1 ]]; then
     while read line; do
         ./_php-bottles-make.sh $line && ./_php-bottles-upload.sh $line || echo "Failed to create bottles for $line"
-    done < <(brew search digitalspacestdio/php | grep 'php[0-9]\{2\}$' | awk -F'/' '{ print $3"" }' | sort)
+    done < <(brew search digitalspacestdio/php | grep 'php[5-9]\{1\}[0-9]\{1\}$' | awk -F'/' '{ print $3"" }' | sort)
 else
     ./_php-bottles-make.sh $1 && ./_php-bottles-upload.sh $1 || echo "Failed to create bottles for $1"
 fi
