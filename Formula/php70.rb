0244b5709328b7e9340b6e4f200017d12d43d820
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 15:57:25 +0400

    php70: update 7.0.33_23 bottle.

--- Formula/php70.rb
+++ Formula/php70.rb
@@ -9,7 +9,7 @@ class Php70 < AbstractPhp
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php70"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8bb502a95bc291de9e243a73bbed033f6c50f67f4e66c52e11d34a615e0002dd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f34432205537f68ec16c01a1c8e59ba0bc3ede3d96ec79adbe285507604ce585"
     sha256 cellar: :any_skip_relocation, ventura:       "925dffa6c0f63773595bd2ba9b4e5ae257793999a3d65ac566ffd534c4823154"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "6298a39d9b1fbc665566988f86931bd008996e7506628ea3865f342df8b51919"
   end
