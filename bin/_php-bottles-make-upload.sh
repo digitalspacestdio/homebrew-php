ba9860f25f037a8abb7bcb54bed7da4a85313ca7
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 15:47:16 +0400

    bottles

--- bin/_php-bottles-make-upload.sh
+++ bin/_php-bottles-make-upload.sh
@@ -6,7 +6,7 @@ cd "${DIR}"
 
 FORMULAS=${@:-$(brew search digitalspacestdio/php | grep 'php[7-9]\{1\}[0-9]\{1\}$' | awk -F'/' '{ print $3 }' | sort)}
 
-for formula in "$FORMULAS"; do
+for formula in $FORMULAS; do
     echo "---> Starting $formula"
     ./_php-bottles-make.sh $formula && {
         ./_php-bottles-upload.sh $formula || echo "Failed to upload bottles for $formula"
