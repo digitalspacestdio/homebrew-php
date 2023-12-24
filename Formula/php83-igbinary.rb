2c4ae2a2c620e9784f3b27ee1629be1914afe4f5
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 21:06:40 +0400

    php83-igbinary: update 3.2.14_1 bottle.

--- Formula/php83-igbinary.rb
+++ Formula/php83-igbinary.rb
@@ -12,7 +12,7 @@ class Php83Igbinary < AbstractPhp83Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php83"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7cca3bb03eaeba469d4f74211b7027c51a4f643532ea4f24eeb356d885450378"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b557566dc8b8bed1b6ef97ae894b10f4347e8a97552a37f8d0b74457243c650c"
     sha256 cellar: :any_skip_relocation, ventura:       "12a559cfbf887085ca3852d451b57f27731360ff29ecea68b416c910b931e911"
   end
 
