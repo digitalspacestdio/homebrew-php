699841bf0592c87b51f3d55093bb16e3ba7c8c75
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 17:07:49 +0400

    php80-sodium: update 8.0.30_1 bottle.

--- Formula/php80-sodium.rb
+++ Formula/php80-sodium.rb
@@ -11,7 +11,7 @@ class Php80Sodium < AbstractPhp80Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php80"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "4a55bb52552a25073a01806177db1e0324e044fcee781f0fc7c1a3520bd333e4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e32b4c2840f21b1be385cb311832afc39bab74e42cf26e37a7b54c8565798741"
     sha256 cellar: :any_skip_relocation, ventura:       "b44baf933fc28868872a4e850b239457c4de1d86cfe09970763e641d331c9490"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "8261f8604f7e928c16d071220a965cf42e157557048589b78fd3d2ffbf9f2080"
   end
