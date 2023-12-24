1bf99bc85cbb4c6815b37835a751771f6fffd280
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 14:41:09 +0400

    php70-gmp: update 7.0.33_18 bottle.

--- Formula/php70-gmp.rb
+++ Formula/php70-gmp.rb
@@ -12,7 +12,7 @@ class Php70Gmp < AbstractPhp70Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php70"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8b74146fd00ec6411bc9ebb2eb1b99f81ed23ab469103221da60baf63385928c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "da8c0807a3ac56448ccbc7b26eeea5f2be201bdb325cd7a97c5af1307603769c"
     sha256 cellar: :any_skip_relocation, ventura:       "61182ed029a159879b252f9b3f57819916c8713acbd6a8e5b326f91322511ae4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "e36863ab95ed448042852c48614b6201f79e33bc280974f91a5cadec0e0d9d21"
   end
