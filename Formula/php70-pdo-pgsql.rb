0366d230e2ee4785764a8ac7cd01397dcecd30f9
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 15:31:11 +0400

    php70-pdo-pgsql: update 7.0.33_19 bottle.

--- Formula/php70-pdo-pgsql.rb
+++ Formula/php70-pdo-pgsql.rb
@@ -12,7 +12,7 @@ class Php70PdoPgsql < AbstractPhp70Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php70"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "cba7827ee6ff304db63bef511113f346c03f468f7ccdd74d9f1361a258c193ae"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e8541b3c7718039c87398feb9fbf64e5c62b44e7accfa8c025f311a590459f28"
     sha256 cellar: :any_skip_relocation, ventura:       "ef84e1948529da6f6cf4134a1a439688be0a540e2de99b631158d4921e260681"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6f2010c55556e9b5dc5e19f61eaa51770f7b40b4073f9e358c518db9db70ada"
   end
