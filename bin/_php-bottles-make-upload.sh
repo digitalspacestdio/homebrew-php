8fc7a93045a16b6a4599a0a86511e55b422136c8
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 15:13:55 +0400

    bottles

--- bin/_php-bottles-make-upload.sh
+++ bin/_php-bottles-make-upload.sh
@@ -5,7 +5,10 @@ cd "${DIR}"
 
 if [[ -z $1 ]]; then
     while read line; do
-        ./_php-bottles-make.sh $line && ./_php-bottles-upload.sh $line || echo "Failed to create bottles for $line"
+        ./_php-bottles-make.sh $line && {
+            ./_php-bottles-upload.sh $line || echo "Failed to upload bottles for $line"
+        } || echo "Failed to build bottles for $line"
+        
     done < <(brew search digitalspacestdio/php | grep 'php[7-9]\{1\}[0-9]\{1\}$' | awk -F'/' '{ print $3"" }' | sort)
 else
     ./_php-bottles-make.sh $1 && ./_php-bottles-upload.sh $1 || echo "Failed to create bottles for $1"
