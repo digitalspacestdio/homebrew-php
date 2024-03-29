21fdc2c68818cffc6b6fa107e78215dc7681296d
scherepanov <sergey@cherepanov.org.ua>
2024-03-29 20:17:50 +0000

    BottlePhpTools

--- Formula/bottle-php-tools.rb
+++ Formula/bottle-php-tools.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class BottlePhpTools < Formula
   url "https://github.com/digitalspacestdio/homebrew-php.git", :using => :git
-  version "0.1.10"
+  version "0.1.11"
 
   depends_on "s3cmd"
   depends_on "jq"
