81f57836630c3bfeea9e6cfe79cce4c3a50fb3bc
Serhii Cherepanov <scherepanov@Serhiis-MacBook-Pro.local>
2024-04-01 20:35:51 +0400

    bottles update

--- Formula/php80-gmp.rb
+++ Formula/php80-gmp.rb
@@ -12,9 +12,10 @@ class Php80Gmp < AbstractPhp80Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php80"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "e39266f71217dbaa9cecf19e6efaf0cfdc31e691c69e6aebb5f6384c0aeb1a34"
-    sha256 cellar: :any_skip_relocation, sonoma:       "94833c197106526e4c884082909b80303c2802ce2d41f16737f50e4a25062998"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "83d6c174da6ebe60dadbe2e61482343c3969e6db35a38b73cd0db44a6acd127a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e39266f71217dbaa9cecf19e6efaf0cfdc31e691c69e6aebb5f6384c0aeb1a34"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4729e8d7d90f33e05beb51383f725df870ae02cb18f419b11031352d6f60006c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "94833c197106526e4c884082909b80303c2802ce2d41f16737f50e4a25062998"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83d6c174da6ebe60dadbe2e61482343c3969e6db35a38b73cd0db44a6acd127a"
   end
 
   depends_on "gmp"
