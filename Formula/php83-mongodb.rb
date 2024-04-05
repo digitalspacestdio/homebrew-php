79f00be089df845c831e1537a878a49f6f3bacc4
scherepanov <s@cherepanov.co>
2024-04-05 21:27:12 +0400

    bottles update

--- Formula/php83-mongodb.rb
+++ Formula/php83-mongodb.rb
@@ -12,6 +12,7 @@ class Php83Mongodb < AbstractPhp83Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php83-mongodb"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "5ec94bbc085417e30744b9e4cd5b9bfc2cb45c592939d8ae3f1677560bc6ef28"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ce87d2035515b7e76aa9668f020f345b607b929164029b35bf88201424facd10"
     sha256 cellar: :any_skip_relocation, sonoma:        "770bd88e37c9f74625ee45da187044a75e9ae5543d7e8bc34b7e98cfa1a8177b"
     sha256 cellar: :any_skip_relocation, monterey:      "198ec3dfb0c997935363e7d3bf276fa2ff82a74d7d922046cf5969ca09d2cfd6"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "1643ceefbe153c9885a142a131a3d6769e30f8a9a733de489cc23fb5e87f1016"
