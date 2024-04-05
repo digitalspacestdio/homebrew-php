775fd7c9b1b4b1c4086a6038b8601a767ffe5325
scherepanov <sergey@cherepanov.org.ua>
2024-04-05 14:06:33 +0000

    bottles update

--- Formula/php72-common.rb
+++ Formula/php72-common.rb
@@ -4,5 +4,12 @@ class Php72Common < AbstractPhpCommon
   include AbstractPhpVersion::Php72Defs
   revision PHP_REVISION
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72-common"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4772499f1a81464299b7eb411e607770fcdf9f365b407860ab96deb897926ece"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cfde4ed4c7a01acd51ddb18c3e9edb9fd437c2e3865d7726fa4b23bf1e440485"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f555224603f06fa1268f9bb5a35f0d3b9b849ae10fb3e96d72ffab4dc3bcc79c"
+  end
+
   init PHP_VERSION_MAJOR, PHP_VERSION, PHP_BRANCH_NUM
 end
