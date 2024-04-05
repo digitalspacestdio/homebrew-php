197c94f3ad412ee6a6fda6ff75fcc6294a32b467
scherepanov <s@cherepanov.co>
2024-04-05 19:04:16 +0400

    bottles update

--- Formula/php56-common.rb
+++ Formula/php56-common.rb
@@ -6,6 +6,7 @@ class Php56Common < AbstractPhpCommon
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php56-common"
+    sha256 cellar: :any_skip_relocation, monterey:     "f57c000865db86afd4265075250e080b5e9732fb6fde6225fb1a614e491341db"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "ccf4e06af2d497f945f8f051f846b472a52118ac8bebb12d8ac7b35ed670f6e2"
   end
 
