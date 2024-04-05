d7bf954b64f36eb05c7b5bb6dbd258d280e4652a
scherepanov <s@cherepanov.co>
2024-04-05 19:05:35 +0400

    bottles update

--- Formula/php70-common.rb
+++ Formula/php70-common.rb
@@ -8,6 +8,7 @@ class Php70Common < AbstractPhpCommon
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php70-common"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "7000bfb7239e3a80a0d9233042bd1068de522956b3ec06fd6f54d771e8726130"
     sha256 cellar: :any_skip_relocation, sonoma:        "a841ad5cee255f388aa200345dc24f7d8d42b917440154e91feac5ef62cd520d"
+    sha256 cellar: :any_skip_relocation, monterey:      "3825be975ffe6cc88e502854f61e1c8726f714e0fc59c9d0db9c6ffe2c2b7898"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea867908fb124d7059a6475a456e39befa3de43429cd917f9ae49c4ece0979de"
   end
   
