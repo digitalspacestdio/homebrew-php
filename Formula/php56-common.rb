2765afe22c198145edd8896dd0964334eb5dc899
scherepanov <sergey@cherepanov.org.ua>
2024-04-05 14:01:03 +0000

    bottles update

--- Formula/php56-common.rb
+++ Formula/php56-common.rb
@@ -4,5 +4,10 @@ class Php56Common < AbstractPhpCommon
   include AbstractPhpVersion::Php56Defs
   revision PHP_REVISION
 
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php56-common"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d4184143a57c444c7b7e83e999f55ab3f49c32f7a07ed1f062a2321d7da159f3"
+  end
+
   init PHP_VERSION_MAJOR, PHP_VERSION, PHP_BRANCH_NUM
 end
