b49a9f693a4afa0e97b7a996dd4a50d2ed72e394
scherepanov <sergey@cherepanov.org.ua>
2024-04-05 14:05:19 +0000

    bottles update

--- Formula/php70-common.rb
+++ Formula/php70-common.rb
@@ -3,6 +3,13 @@ require File.expand_path("../../Abstract/abstract-php-common", __FILE__)
 class Php70Common < AbstractPhpCommon
   include AbstractPhpVersion::Php70Defs
   revision PHP_REVISION
+
+  bottle do
+    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php70-common"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7000bfb7239e3a80a0d9233042bd1068de522956b3ec06fd6f54d771e8726130"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a841ad5cee255f388aa200345dc24f7d8d42b917440154e91feac5ef62cd520d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea867908fb124d7059a6475a456e39befa3de43429cd917f9ae49c4ece0979de"
+  end
   
   init PHP_VERSION_MAJOR, PHP_VERSION, PHP_BRANCH_NUM
 end
