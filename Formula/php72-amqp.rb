2788bbc0cd9e5ea659927eebb385088236b1a060
scherepanov <s@cherepanov.co>
2024-04-10 22:36:51 +0000

    revision

--- Formula/php72-amqp.rb
+++ Formula/php72-amqp.rb
@@ -4,17 +4,10 @@ class Php72Amqp < AbstractPhp72Extension
   init
   desc "PHP extension to communicate with any AMQP compliant server."
   homepage "https://github.com/php-amqp/php-amqp"
-  url "https://github.com/php-amqp/php-amqp/archive/refs/tags/v2.1.2.tar.gz"
-  sha256 "5eebe1d0414af8e4c1e1b5040be68168a533704f09f6f66e4d48ab78edd1d8d2"
+  url "https://github.com/php-amqp/php-amqp/archive/refs/tags/v1.11.0.tar.gz"
+  sha256 "b74de00674ee2f75a4c31f9af2475b9296d10d06549bdf605ca9d24c19510ca3"
   head "https://github.com/pdezwart/php-amqp.git"
   revision PHP_REVISION
-
-  bottle do
-    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d063ec06c7238adc2dd976c0f3c7120f4dd2c7aadcc02c3e42ced5cdf395ee3d"
-    sha256 cellar: :any_skip_relocation, monterey:      "637d40cfd8c5e2fa59ffbda6bcf0f2f8f8852a8f6f069d1287f70175b88d4b26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e7fe03b3ceadda94c234e28d92716e3ef9ba3a9197f75fc20bf01da12af512f"
-  end
 
   depends_on "rabbitmq-c"
 
--- Formula/php73-amqp.rb
+++ Formula/php73-amqp.rb
@@ -4,17 +4,10 @@ class Php73Amqp < AbstractPhp73Extension
   init
   desc "PHP extension to communicate with any AMQP compliant server."
   homepage "https://github.com/php-amqp/php-amqp"
-  url "https://github.com/php-amqp/php-amqp/archive/refs/tags/v2.1.2.tar.gz"
-  sha256 "5eebe1d0414af8e4c1e1b5040be68168a533704f09f6f66e4d48ab78edd1d8d2"
+  url "https://github.com/php-amqp/php-amqp/archive/refs/tags/v1.11.0.tar.gz"
+  sha256 "b74de00674ee2f75a4c31f9af2475b9296d10d06549bdf605ca9d24c19510ca3"
   head "https://github.com/pdezwart/php-amqp.git"
   revision PHP_REVISION
-
-  bottle do
-    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php73"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3267253b94468bb72cbbf8034b32933f19a150162e0df27ced62adbc137b2400"
-    sha256 cellar: :any_skip_relocation, monterey:      "931aa12c22e138baeb085639622baae9cbc9e95e3f3bc34f44095e5f3002135e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb5dd64beaea847d676a6c7c8af5ec32f03d617c81d3c31f44f1bae701104f47"
-  end
 
   depends_on "rabbitmq-c"
 
