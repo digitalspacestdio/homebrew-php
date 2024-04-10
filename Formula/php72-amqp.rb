105e521ee3596120850f650ffd498172197e041a
Serhii Cherepanov <scherepanov@Serhiis-MacBook-Pro.local>
2024-04-11 02:48:05 +0400

    bottles update: php72-amqp

--- Formula/php72-amqp.rb
+++ Formula/php72-amqp.rb
@@ -11,8 +11,9 @@ class Php72Amqp < AbstractPhp72Extension
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php72-amqp"
-    sha256 cellar: :any_skip_relocation, monterey:     "23aac54485349eb2f0297494d31f82897f9befcd50b2cb1e91547044bacae8f6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d238899cf150e2e06f188b08cfb70ecba3f5d4707aa12d64c0298d0bf0a3c407"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "589523cd8373bfa75a0d1428f81a9297d9f24c59d4027c24001bfded9c352774"
+    sha256 cellar: :any_skip_relocation, monterey:      "23aac54485349eb2f0297494d31f82897f9befcd50b2cb1e91547044bacae8f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d238899cf150e2e06f188b08cfb70ecba3f5d4707aa12d64c0298d0bf0a3c407"
   end
 
   depends_on "rabbitmq-c"
