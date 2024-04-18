ef020e1f492b4cbc52c69ac39ee214d37302bf62
scherepanov <s@cherepanov.co>
2024-04-18 18:21:55 +0400

    bottles update: php73-msmtp

--- Formula/php73-msmtp.rb
+++ Formula/php73-msmtp.rb
@@ -11,6 +11,7 @@ class Php73Msmtp < AbstractPhp73Extension
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php73-msmtp"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "2d98424f9f1c79de3957a7992691e4881ffb27d0defa7a26802e8f6ffb82006a"
+    sha256 cellar: :any_skip_relocation, monterey:      "8ccc75c2a311dff2d1c59d4ec26c541e76638b537e71c0c00c6abe2fcb229f3b"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "8416ec7c6622b9d05aba178afc33947064800c1e8c1bb1ed57efbfec575db0d8"
   end
 
