75ee3c06577f2fd5e433455f484c785d7776d4b8
scherepanov <s@cherepanov.co>
2024-04-19 00:09:48 +0400

    bottles update: php81-common

--- Formula/php81-common.rb
+++ Formula/php81-common.rb
@@ -6,7 +6,8 @@ class Php81Common < AbstractPhpCommon
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php81-common"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5229fd3a367fae83d7cde16b3ef6e4e4670247138af71e4fc16e006bcff59111"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7103a1050dd5fc0affd167ffe157fe4fc121daea2d08d3f765d35675deea0186"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5229fd3a367fae83d7cde16b3ef6e4e4670247138af71e4fc16e006bcff59111"
   end
 
   init PHP_VERSION_MAJOR, PHP_VERSION, PHP_BRANCH_NUM
