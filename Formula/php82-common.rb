4c58e572d5bfe621a3b45d660a03b1fe1e43bfdc
scherepanov <s@cherepanov.co>
2024-04-19 00:12:28 +0400

    bottles update: php82-common

--- Formula/php82-common.rb
+++ Formula/php82-common.rb
@@ -7,6 +7,7 @@ class Php82Common < AbstractPhpCommon
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/php82-common"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "1f3738e4f40079d3d01c863be5bd98f301e55222852febfffded59e116a49422"
+    sha256 cellar: :any_skip_relocation, monterey:      "c95db9ef89979fa721605fc8792722a0c124142e1f8ff3e2f650d5e9d98316c3"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a4f6de9c1c2d845dc542811bcec96569bfc1e835978718777d5d3d7aa54553ea"
   end
 
