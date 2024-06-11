d200591423c558ff337a3b82ce170a6d0ba07bf8
scherepanov <s@cherepanov.co>
2024-06-11 10:14:20 +0000

    modified:   bin/_php-update-versions.sh

--- bin/_php-update-versions.sh
+++ bin/_php-update-versions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 set -e
 if [[ ! -z $DEBUG ]]; then set -x; fi
 #pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
@@ -6,10 +7,10 @@ if [[ ! -z $DEBUG ]]; then set -x; fi
 brew tap digitalspacestdio/common
 brew tap digitalspacestdio/php
 cd $(brew tap-info --json digitalspacestdio/php | jq -r '.[].path')
-git stash
+#git stash
 #git pull
 DIR=$(pwd -P)
-. ../.vars
+source $DIR/.vars
 
 for VERSION in "5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2" "8.3"
 do
@@ -18,10 +19,10 @@ PHP_VERSION=$(echo ${RELEASES} | jq -r '."version"')
 PHP_VERSION_MINOR=$(echo $PHP_VERSION | awk -F. '{ print $1 "." $2 }')
 PHP_TARBZ2_FILENAME=$(echo ${RELEASES} | jq -r '.source[] | select(.filename | contains("tar.bz2")) | .filename')
 PHP_TARBZ2_SHA256=$(echo ${RELEASES} | jq -r '.source[] | select(.filename | contains("tar.bz2")) | .sha256')
-PHP_BRANCH_NUM=$(echo $PHP_VERSION | awk -F. '{ print $1 $2 }')\
+PHP_BRANCH_NUM=$(echo $PHP_VERSION | awk -F. '{ print $1 $2 }')
 PHP_REVISION=$(bash -c 'echo $PHP_REVISION_'$PHP_BRANCH_NUM)
 
-export CONFIG='module Php'$PHP_VERSION_MINOR'Defs
+export CONFIG='module Php'$PHP_BRANCH_NUM'Defs
     PHP_SRC_TARBALL = "https://php.net/get/'$PHP_TARBZ2_FILENAME'/from/this/mirror".freeze
     PHP_GITHUB_URL  = "https://github.com/php/php-src.git".freeze
     PHP_VERSION_MAJOR = "'$PHP_VERSION_MINOR'".freeze
