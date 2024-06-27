69fccf260390be592e4bc9ff1102ea530d6b145f
scherepanov <s@cherepanov.co>
2024-06-27 14:19:03 +0000

    update script

--- bin/_php-bottles-upload.sh
+++ bin/_php-bottles-upload.sh
@@ -19,6 +19,23 @@ for PHP_FORMULA in $FORMULAS; do
     cd ${HOME}/.bottles/$PHP_FORMULA.bottle
     ls | grep $PHP_FORMULA'.*--.*.gz$' | awk -F'--' '{ print $0 " " $1 "-" $2 }' | xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} bash -c 'mv {}'
     ls | grep $PHP_FORMULA'.*--.*.json$' | awk -F'--' '{ print $0 " " $1 "-" $2 }' | xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} bash -c 'mv {}'
+
+    for jsonfile in ./*.json; do
+        jsonfile=$(basename $jsonfile)
+        JSON_FORMULA_NAME=$(jq -r '.[].formula.name' "$jsonfile")
+        if ! [[ -z $JSON_FORMULA_NAME ]]; then
+            while read tgzName; do
+                if [[ -f "$tgzName" ]]; then
+                    s3cmd info "s3://homebrew-bottles/$PHP_FORMULA/$tgzName" >/dev/null && {
+                        echo "File already exists on remote storage 's3://homebrew-bottles/$PHP_FORMULA/$tgzName'"
+                        echo "Terminating..."
+                        exit 1
+                    }
+                fi
+            done < <(jq -r '."digitalspacestdio/php/'$JSON_FORMULA_NAME'".bottle.tags[].filename' "$jsonfile")
+        done
+    done
+
     for jsonfile in ./*.json; do
         jsonfile=$(basename $jsonfile)
         JSON_FORMULA_NAME=$(jq -r '.[].formula.name' "$jsonfile")
@@ -26,9 +43,6 @@ for PHP_FORMULA in $FORMULAS; do
             mergedfile=$(jq -r '.["digitalspacestdio/php/'$JSON_FORMULA_NAME'"].formula.name + "-" + ."digitalspacestdio/php/'$JSON_FORMULA_NAME'".formula.pkg_version + ".json"' "$jsonfile")
             while read tgzName; do
                 if [[ -f "$tgzName" ]]; then
-                    s3cmd info "s3://homebrew-bottles/$PHP_FORMULA/$tgzName" >/dev/null && {
-                        s3cmd del "s3://homebrew-bottles/$PHP_FORMULA/$tgzName"
-                    } || /usr/bin/true
                     s3cmd put "$tgzName" "s3://homebrew-bottles/$PHP_FORMULA/$tgzName"
                 fi
             done < <(jq -r '."digitalspacestdio/php/'$JSON_FORMULA_NAME'".bottle.tags[].filename' "$jsonfile")
