9261fe4828c20da3690a7c5c8c1191d8ca6b87a1
scherepanov <s@cherepanov.co>
2024-08-25 15:30:03 +0000

    bottles update: php84

--- Formula/php84-common.rb
+++ Formula/php84-common.rb
@@ -5,9 +5,8 @@ class Php84Common < AbstractPhpCommon
   revision PHP_REVISION
 
   bottle do
-    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/php/8.4.0-100"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7be034f50f9169b2778e6ed93f889f1b4750186be33fd3c106524614d335c32c"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "25dfb2e32691997ec1c11dd6e0c148ed963cc69c410e3c056066ca2dd82bf61d"
+    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/php/8.4.0beta3-100"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2335c95e362793cd5d5f78d123f4f2ce3bf81c9854ff7aeff33f49d92d5a2821"
   end
   
   init PHP_VERSION_MAJOR, PHP_VERSION, PHP_BRANCH_NUM
