7347bb076ddd3233c073b14a9d690e0ea6ae3601
scherepanov <s@cherepanov.co>
2024-09-23 16:57:08 +0000

    ext-rdkafka

--- Formula/php70-rdkafka.rb
+++ Formula/php70-rdkafka.rb
@@ -4,20 +4,29 @@ class Php70Rdkafka < AbstractPhp70Extension
   init
   desc "PHP extension for Apache Kafka (php-rdkafka)"
   homepage "https://github.com/arnaud-lb/php-rdkafka"
-  url "https://pecl.php.net/get/rdkafka-3.0.4.tgz"
-  sha256 "4cb0a37664810d2a338cf5351e357be7294d99458f76435801e0ed5e328dc5ee"
+  url "https://pecl.php.net/get/rdkafka-6.0.3.tgz"
+  sha256 "12eaab976d49697e31f1638b47889eb5ec61adb758708941112b157f8ec7dd48"
   head "https://github.com/arnaud-lb/php-rdkafka.git"
   revision PHP_REVISION
 
-  depends_on "librdkafka"
+  resource "librdkafka" do
+    url "https://github.com/confluentinc/librdkafka/archive/refs/tags/v2.5.3.tar.gz"
+    sha256 "eaa1213fdddf9c43e28834d9a832d9dd732377d35121e42f875966305f52b8ff"
+  end
 
   def install
+    resource("librdkafka").stage do
+      args = []
+      args << "--prefix=#{prefix}/librdkafka"
+      args << "--mandir=#{man}"
+
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
     Dir.chdir "rdkafka-#{version}" unless build.head?
-
-    # ENV.universal_binary if build.universal?
-
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "./configure", "--prefix=#{prefix}", "--with-rdkafka=#{prefix}/librdkafka", phpconfig
     system "make"
     prefix.install "modules/rdkafka.so"
     write_config_file if build.with? "config-file"
--- Formula/php83-rdkafka.rb
+++ Formula/php83-rdkafka.rb
@@ -4,18 +4,29 @@ class Php83Rdkafka < AbstractPhp83Extension
   init
   desc "PHP extension for Apache Kafka (php-rdkafka)"
   homepage "https://github.com/arnaud-lb/php-rdkafka"
-  url "https://github.com/arnaud-lb/php-rdkafka/archive/refs/tags/6.0.3.tar.gz"
-  sha256 "058bac839a84f773c931776e7f6cbfdb76443849d3ea2b43ba43b80f64df7453"
+  url "https://pecl.php.net/get/rdkafka-6.0.3.tgz"
+  sha256 "12eaab976d49697e31f1638b47889eb5ec61adb758708941112b157f8ec7dd48"
   head "https://github.com/arnaud-lb/php-rdkafka.git"
   revision PHP_REVISION
 
-  depends_on "librdkafka"
+  resource "librdkafka" do
+    url "https://github.com/confluentinc/librdkafka/archive/refs/tags/v2.5.3.tar.gz"
+    sha256 "eaa1213fdddf9c43e28834d9a832d9dd732377d35121e42f875966305f52b8ff"
+  end
 
   def install
-    Dir.chdir "rdkafka-#{version}" unless build.head?
+    resource("librdkafka").stage do
+      args = []
+      args << "--prefix=#{prefix}/librdkafka"
+      args << "--mandir=#{man}"
 
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+    Dir.chdir "rdkafka-#{version}" unless build.head?
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "./configure", "--prefix=#{prefix}", "--with-rdkafka=#{prefix}/librdkafka", phpconfig
     system "make"
     prefix.install "modules/rdkafka.so"
     write_config_file if build.with? "config-file"
