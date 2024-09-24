5b9e4f72e63a506b3c8bf99632c74a674b854634
scherepanov <s@cherepanov.co>
2024-09-24 08:24:03 +0000

    ext-rdkafka

--- Formula/php56-rdkafka.rb
+++ Formula/php56-rdkafka.rb
@@ -4,20 +4,21 @@ class Php56Rdkafka < AbstractPhp56Extension
   init
   desc "PHP extension for Apache Kafka (php-rdkafka)"
   homepage "https://github.com/arnaud-lb/php-rdkafka"
-  url "https://pecl.php.net/get/rdkafka-1.0.0.tgz"
-  sha256 "3a8957c618f9f4093c8258621841c543270f4c4cbfd8ee687ca1dc270b244f87"
+  url "https://pecl.php.net/get/rdkafka-4.1.2.tgz"
+  sha256 "8ae04c240ce810bc08c07ea09f90daf9df72f0dde220df460985945a3ceec7fc"
   head "https://github.com/arnaud-lb/php-rdkafka.git"
   revision PHP_REVISION
 
+  depends_on "pcre2"
   depends_on "librdkafka"
 
   def install
+    ENV.append "LDFLAGS", "-L#{Formula["pcre2"].opt_prefix}/lib"
+    ENV.append "CPPFLAGS", "-I#{Formula["pcre2"].opt_prefix}/include"
+
     Dir.chdir "rdkafka-#{version}" unless build.head?
-
-    # ENV.universal_binary if build.universal?
-
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "./configure", "--prefix=#{prefix}", "--with-rdkafka=#{Formula["librdkafka"].opt_prefix}", phpconfig
     system "make"
     prefix.install "modules/rdkafka.so"
     write_config_file if build.with? "config-file"
--- Formula/php81-rdkafka.rb
+++ Formula/php81-rdkafka.rb
@@ -10,31 +10,15 @@ class Php81Rdkafka < AbstractPhp81Extension
   revision PHP_REVISION
 
   depends_on "pcre2"
-
-  resource "librdkafka" do
-    url "https://github.com/confluentinc/librdkafka/archive/refs/tags/v2.5.3.tar.gz"
-    sha256 "eaa1213fdddf9c43e28834d9a832d9dd732377d35121e42f875966305f52b8ff"
-  end
+  depends_on "librdkafka"
 
   def install
-    resource("librdkafka").stage do\
-      ENV.append "CFLAGS", "-Wno-incompatible-pointer-types"
-      ENV.append "CFLAGS", "-Wno-deprecated-declarations"
-      args = []
-      args << "--prefix=#{prefix}/librdkafka"
-      args << "--mandir=#{man}"
-
-      system "./configure", *args
-      system "make"
-      system "make", "install"
-    end
-
     ENV.append "LDFLAGS", "-L#{Formula["pcre2"].opt_prefix}/lib"
     ENV.append "CPPFLAGS", "-I#{Formula["pcre2"].opt_prefix}/include"
 
     Dir.chdir "rdkafka-#{version}" unless build.head?
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", "--with-rdkafka=#{prefix}/librdkafka", phpconfig
+    system "./configure", "--prefix=#{prefix}", "--with-rdkafka=#{Formula["librdkafka"].opt_prefix}", phpconfig
     system "make"
     prefix.install "modules/rdkafka.so"
     write_config_file if build.with? "config-file"
