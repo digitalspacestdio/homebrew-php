087fa7f2b07ff677f81c4e14cae98affe420b3fc
scherepanov <s@cherepanov.co>
2024-09-26 12:42:58 +0000

    modified:   Formula/php56-event.rb

--- Formula/php56-event.rb
+++ Formula/php56-event.rb
@@ -10,7 +10,7 @@ class Php56Event < AbstractPhp56Extension
   revision PHP_REVISION
   
   depends_on "libevent"
-  depends_on "openssl@1.0"
+  depends_on "openssl@1.1"
 
   def install
     Dir.chdir "event-#{version}" unless build.head?
